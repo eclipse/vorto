7d0dc7244f1f50134d25a1dec2168db6d0fd7f27
Erle Czar Mantos <ErleCzar.Mantos@bosch-si.com>
2015-07-29 16:48:07 +0800

    Add repository extension point and REST implementation to repository extension point.
    Signed-off-by: Erle Czar Mantos <ErleCzar.Mantos@bosch-si.com>
    
    Signed-off-by: Steve Harmon <stephen.harmon@thingworx.com>

---
+++ bundles/org.eclipse.vorto.repository/src/org/eclipse/vorto/repository/RestModelRepositoryExtensionFactory.java
@@ -0,0 +1,31 @@
+package org.eclipse.vorto.repository;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IExecutableExtensionFactory;
+
+public class RestModelRepositoryExtensionFactory implements
+		IExecutableExtensionFactory {
+
+	@Override
+	public Object create() throws CoreException {
+		RestModelRepository modelRepository = new RestModelRepository(new ConnectionInfoSupplier() {
+			public String connectionUrl() {
+				return "http://localhost:8080/infomodelrepository";
+			}
+		});
+		
+		modelRepository.addObserver(new Observer() {
+			public void update(Observable o, Object arg) {
+				//MessageDisplayFactory.getMessageDisplay().display((String) arg);
+				// TODO : find a way to display messages on Console here
+				System.out.println((String) arg);
+			}			
+		});
+		
+		return modelRepository;
+	}
+
+}
