ccc58096c5a3390061a769799775dcdd29df1c3b
Olaf Weinmann <olaf.weinmann@bosch-si.com>
2016-02-25 17:04:47 +0100

    Initial Version of a LaTeX code generator.

---
+++ example-generators/org.eclipse.vorto.codegen.examples.latex/src/org/eclipse/vorto/codegen/examples/latex/tasks/LatexInformationModelGeneratorTask.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ *  Copyright (c) 2015 Bosch Software Innovations GmbH and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Eclipse Distribution License v1.0 which accompany this distribution.
+ *   
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  The Eclipse Distribution License is available at
+ *  http://www.eclipse.org/org/documents/edl-v10.php.
+ *   
+ *  Contributors:
+ *  Bosch Software Innovations GmbH - Please refer to git log
+ *******************************************************************************/
+package org.eclipse.vorto.codegen.examples.latex.tasks;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.vorto.codegen.api.AbstractTemplateGeneratorTask;
+import org.eclipse.vorto.codegen.api.ITemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexComplexPropertyTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexEntityTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexEnumTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexFBPropertyTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexFunctionBlockTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexOperationTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexSimplePropertyConstraintTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.LatexSimplePropertyTemplate;
+import org.eclipse.vorto.codegen.examples.latex.tasks.template.ReviewInformationModelTemplate;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+
+public class LatexInformationModelGeneratorTask extends
+	AbstractTemplateGeneratorTask<InformationModel> {
+	
+	private String markdownFileExtension;
+	private String targetPath;
+	
+	public LatexInformationModelGeneratorTask (String markdownFileExtension, 
+			String targetPath) {
+		this.markdownFileExtension = markdownFileExtension;
+		this.targetPath = targetPath;
+	}
+	
+	@Override
+	public String getFileName(InformationModel im) {
+		return StringUtils.capitalize(im.getName()) + markdownFileExtension;
+	}
+	
+	@Override
+		public String getPath(InformationModel im) {
+		return targetPath;
+	}
+	
+	@Override
+	public ITemplate<InformationModel> getTemplate() {
+		LatexOperationTemplate operationTemplate = new LatexOperationTemplate();
+		LatexFBPropertyTemplate fbPropertyTemplate = new LatexFBPropertyTemplate();
+		LatexComplexPropertyTemplate complexTemplate = new LatexComplexPropertyTemplate();
+		LatexSimplePropertyTemplate simpleTemplate = new LatexSimplePropertyTemplate();
+		LatexSimplePropertyConstraintTemplate constraintTemplate = new LatexSimplePropertyConstraintTemplate();
+		LatexFunctionBlockTemplate fbTemplate = new LatexFunctionBlockTemplate(constraintTemplate, simpleTemplate, complexTemplate, operationTemplate);
+		LatexEntityTemplate entityTemplate = new LatexEntityTemplate(constraintTemplate, simpleTemplate, complexTemplate);
+		LatexEnumTemplate enumTemplate = new LatexEnumTemplate();
+		
+		return new ReviewInformationModelTemplate(fbPropertyTemplate, fbTemplate, entityTemplate, enumTemplate);
+	}
+}
