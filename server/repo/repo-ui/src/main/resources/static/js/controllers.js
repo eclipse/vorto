425a9395f74f1eab19d2b441a9c72d4698964b31
aedelmann <alexander.edelmann@bosch-si.com>
2016-09-27 04:12:58 +0800

    Fixed bug in eclipse toolset to exclude mappings in repository search result

--- server/repo/repo-ui/src/main/resources/static/js/controllers.js
+++ server/repo/repo-ui/src/main/resources/static/js/controllers.js
@@ -20,7 +20,7 @@ repositoryControllers.controller('SearchController', [ '$scope','$http', '$locat
   	$scope.search = function() {
   		var filter = null;
   		if ($scope.modelType === 'all') {
-  			filter = $scope.queryFilter + "-Mapping";
+  			filter = $scope.queryFilter + " -Mapping";
   		} else {
   			filter = $scope.queryFilter + " -Mapping "+$scope.modelType;
   		}
