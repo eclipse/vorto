e267512b363bbbfb4f1756cc2af7db6e2b8bf13c
Erle Czar Mantos <ErleCzar.Mantos@bosch-si.com>
2017-01-23 16:13:02 +0800

    Extract specific file from generated output using ! notation. Fixes #489
    Signed-off-by: Erle Czar Mantos <ErleCzar.Mantos@bosch-si.com>

--- server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/web/ModelGenerationController.java
+++ server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/web/ModelGenerationController.java
@@ -15,14 +15,19 @@
 package org.eclipse.vorto.repository.web;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,12 +42,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -58,6 +65,7 @@ import io.swagger.annotations.ApiResponses;
 @RequestMapping(value = "/rest/generation-router")
 public class ModelGenerationController extends AbstractRepositoryController {
 	
+	private static final String ZIPFILE_EXTENSION = ".zip";
 	private static final String ATTACHMENT_FILENAME = "attachment; filename = ";
 	private static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
 	private static final String CONTENT_DISPOSITION = "content-disposition";
@@ -67,9 +75,78 @@ public class ModelGenerationController extends AbstractRepositoryController {
 	@Autowired
 	private IGeneratorService generatorService;
 	
+	@ApiOperation(value = "Generate code for a specified platform, and extract specified path")
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "Wrong input"), @ApiResponse(code = 404, message = "Model not found")})
+	@RequestMapping(value = "/{namespace}/{name}/{version:.+}/{serviceKey:[^!]+}!/**", method = RequestMethod.GET)
+	public void generateAndExtract(@ApiParam(value = "The namespace of vorto model, e.g. com.mycompany", required = true) final @PathVariable String namespace, 
+							@ApiParam(value = "The name of vorto model, e.g. NewInfomodel", required = true) final @PathVariable String name,
+							@ApiParam(value = "The version of vorto model, e.g. 1.0.0", required = true) final @PathVariable String version, 
+							@ApiParam(value = "Service key for a specified platform, e.g. lwm2m", required = true) @PathVariable String serviceKey,
+							final HttpServletRequest request,
+							final HttpServletResponse response) {
+		
+		try {
+			GeneratedOutput generatedOutput = generatorService.generate(new ModelId(name,namespace,version), 
+					URLDecoder.decode(serviceKey, "utf-8"), getRequestParams(request));
+			
+			String extractPath = getExtractPath(request);
+
+			if (extractPath == null || extractPath.trim().isEmpty()) {
+				writeToResponse(response, generatedOutput);
+				return;
+			}
+			 
+			if (generatedOutput.getFileName().endsWith(ZIPFILE_EXTENSION)) {
+				Optional<GeneratedOutput> extractionResult = extractFromZip(generatedOutput.getContent(), extractPath);
+				if (extractionResult.isPresent()) {
+					writeToResponse(response, extractionResult.get());
+					return;
+				}
+			}
+			
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);		
+		} catch (IOException e) {
+			throw new RuntimeException("Error copying file.", e);
+		}	
+	}
+	
+	private Optional<GeneratedOutput> extractFromZip(byte[] zipFile, String filenameInZip) throws IOException {
+		Objects.requireNonNull(zipFile);
+		Objects.requireNonNull(filenameInZip);
+		
+		ZipInputStream zipInputStream = new ZipInputStream(new ByteArrayInputStream(zipFile));
+		ZipEntry ze = null;
+		while ((ze = zipInputStream.getNextEntry()) != null) {
+			if (ze.getName().equals(filenameInZip)) {
+				ByteArrayOutputStream outputStream = new ByteArrayOutputStream(9000);
+				
+				byte[] buffer = new byte[9000];
+				int len = 0;
+				while ((len = zipInputStream.read(buffer)) != -1) {
+					outputStream.write(buffer, 0, len);
+				}
+				
+				byte[] outputBytes = outputStream.toByteArray();
+				
+				return Optional.of(new GeneratedOutput(outputBytes, Paths.get(ze.getName()).getFileName().toString(), outputBytes.length));
+			}
+		}
+		
+		return Optional.empty();
+	}
+
+	private String getExtractPath(final HttpServletRequest request) {
+		String path = (String) request.getAttribute(
+	            HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+	    String bestMatchPattern = (String ) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+	    AntPathMatcher apm = new AntPathMatcher();
+	    return apm.extractPathWithinPattern(bestMatchPattern, path);
+	}
+	
 	@ApiOperation(value = "Generate code for a specified platform")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "Wrong input"), @ApiResponse(code = 404, message = "Model not found")})
-	@RequestMapping(value = "/{namespace}/{name}/{version:.+}/{serviceKey}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{namespace}/{name}/{version:.+}/{serviceKey:[^!]+}", method = RequestMethod.GET)
 	public void generate( 	@ApiParam(value = "The namespace of vorto model, e.g. com.mycompany", required = true) final @PathVariable String namespace, 
 							@ApiParam(value = "The name of vorto model, e.g. NewInfomodel", required = true) final @PathVariable String name,
 							@ApiParam(value = "The version of vorto model, e.g. 1.0.0", required = true) final @PathVariable String version, 
@@ -79,21 +156,32 @@ public class ModelGenerationController extends AbstractRepositoryController {
 		Objects.requireNonNull(namespace, "namespace must not be null");
 		Objects.requireNonNull(name, "name must not be null");
 		Objects.requireNonNull(version, "version must not be null");
+		Objects.requireNonNull(serviceKey, "version must not be null");
 
-		Map<String, String> requestParams = new HashMap<>();
-		request.getParameterMap().entrySet().stream().forEach(x -> requestParams.put(x.getKey(), x.getValue()[0]));
-		
 		try {
-			GeneratedOutput generatedOutput = generatorService.generate(new ModelId(name,namespace,version), URLDecoder.decode(serviceKey, "utf-8"), requestParams);
-			response.setHeader(CONTENT_DISPOSITION, ATTACHMENT_FILENAME + generatedOutput.getFileName());
-			response.setContentLengthLong(generatedOutput.getSize());
-			response.setContentType(APPLICATION_OCTET_STREAM);
-		
-			IOUtils.copy(new ByteArrayInputStream(generatedOutput.getContent()), response.getOutputStream());
-			response.flushBuffer();
+			GeneratedOutput generatedOutput = generatorService.generate(new ModelId(name,namespace,version), URLDecoder.decode(serviceKey, "utf-8"), getRequestParams(request));
+			writeToResponse(response, generatedOutput);
 		} catch (IOException e) {
 			throw new RuntimeException("Error copying file.", e);
 		}
+	}
+
+	private void writeToResponse(final HttpServletResponse response, GeneratedOutput generatedOutput)
+			throws IOException {
+		response.setHeader(CONTENT_DISPOSITION, ATTACHMENT_FILENAME + generatedOutput.getFileName());
+		response.setContentLengthLong(generatedOutput.getSize());
+		response.setContentType(APPLICATION_OCTET_STREAM);
+
+		IOUtils.copy(new ByteArrayInputStream(generatedOutput.getContent()), response.getOutputStream());
+		response.flushBuffer();
+	}
+	
+	private Map<String, String> getRequestParams(final HttpServletRequest request) {
+		Map<String, String> requestParams = new HashMap<>();
+		request.getParameterMap().entrySet().stream().forEach(x -> {
+			requestParams.put(x.getKey(), x.getValue()[0]);
+		});
+		return requestParams;
 	}
 
 	@ApiOperation(value = "Returns all currently registered Code Generator")
