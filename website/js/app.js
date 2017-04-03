235622c8bc65b327257b7d5804bbca3c4e26c968
deepagithub <deepalakshmi.srinivasan@bosch-si.com>
2017-04-03 14:33:50 +0800

    Added a logo for Vorto in the website
    Signed-off-by: deepagithub <deepalakshmi.srinivasan@bosch-si.com>

--- website/js/app.js
+++ website/js/app.js
@@ -1,15 +1,22 @@
  $(function(){
       var headerScroll = 300;
+      var headerScrollA = 80;
       $(window).scroll(function() {
       var scroll = getScroll();
       if ( scroll >= headerScroll ) {
         $('.navbar-lp').addClass('scroll');
         $('.navbar-lp').addClass('scroll-bg');
         }
+        else if(scroll >= headerScrollA) {
+         $('.navbar-lp').addClass('scroll');
+          $('.navbar-lp').addClass('scroll-bg');
+        }
       else {
         $('.navbar-lp').removeClass('scroll');
         $('.navbar-lp').removeClass('scroll-bg');
         }
+
+
 });
 
 
