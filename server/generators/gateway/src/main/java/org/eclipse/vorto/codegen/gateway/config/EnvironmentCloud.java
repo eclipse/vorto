8e9720d125b5f20f0b212ba6a3423982b8efaeed
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-05-15 18:31:20 +0800

    Tweaks for cloud settings.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/config/EnvironmentCloud.java
+++ server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/config/EnvironmentCloud.java
@@ -4,28 +4,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Strings;
+
 @Component
-@Profile("local")
-public class Environment {
+@Profile("cloud")
+public class EnvironmentCloud extends Environment {
+
 	@Value("${vorto.serverUrl}")
 	private String vortoServerUrl;
 	
 	@Value("${server.serviceUrl}")
 	private String applicationServiceUrl;
-
+	
 	public String getVortoRepoUrl() {
+		String vortoRepoUrl = System.getenv("VORTO_REPO_URL");
+		if (!Strings.nullToEmpty(vortoRepoUrl).trim().isEmpty()) {
+			return vortoRepoUrl;
+		}
 		return vortoServerUrl;
 	}
 
-	public void setVortoRepoUrl(String vortoServerUrl) {
-		this.vortoServerUrl = vortoServerUrl;
-	}
-
 	public String getAppServiceUrl() {
+		String serviceoUrl = System.getenv("APP_SERVICE_URL");
+		if (!Strings.nullToEmpty(serviceoUrl).trim().isEmpty()) {
+			return serviceoUrl;
+		}
 		return applicationServiceUrl;
-	}
-
-	public void setAppServiceUrl(String applicationServiceUrl) {
-		this.applicationServiceUrl = applicationServiceUrl;
 	}
 }
--- server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/controllers/Generators.java
+++ server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/controllers/Generators.java
@@ -2,12 +2,15 @@ package org.eclipse.vorto.codegen.gateway.controllers;
 
 import java.io.ByteArrayInputStream;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.vorto.codegen.api.GeneratorServiceInfo;
 import org.eclipse.vorto.codegen.api.IGenerationResult;
+import org.eclipse.vorto.codegen.gateway.config.Environment;
 import org.eclipse.vorto.codegen.gateway.model.Generator;
 import org.eclipse.vorto.codegen.gateway.repository.GeneratorRepository;
 import org.eclipse.vorto.codegen.gateway.service.VortoService;
@@ -24,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest")
 public class Generators {
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private VortoService vorto;
@@ -53,5 +59,24 @@ public class Generators {
 				.header("content-disposition", "attachment; filename = " + result.getFileName())
 				.contentType(MediaType.parseMediaType(result.getMediatype()))
 				.body(new InputStreamResource(new ByteArrayInputStream(result.getContent())));
+	}
+	
+	@RequestMapping(value = "/reset", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, String> reRegister() {
+		Map<String, String> map = new HashMap<String, String>();
+		
+		try {
+			repo.list().stream().forEach(vorto::register);
+			map.put("result", "OK");
+		} catch(Exception e) {
+			map.put("result", "ERROR");
+			map.put("errorMessage", e.getMessage());
+		}
+		
+		map.put("vortoServerUrl", env.getVortoRepoUrl());
+		map.put("applicationServiceUrl", env.getAppServiceUrl());
+		map.put("numGenerators", Integer.toString(repo.list().size()));
+		
+		return map;
 	}
 }
