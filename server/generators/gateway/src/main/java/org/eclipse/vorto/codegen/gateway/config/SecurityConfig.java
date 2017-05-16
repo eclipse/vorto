c92269f493308a2459f2295e641f93bbec9411e7
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-05-16 14:59:39 +0800

    Fix codacy issue 2.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/config/SecurityConfig.java
+++ server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/config/SecurityConfig.java
@@ -21,14 +21,16 @@ public class SecurityConfig {
 	@PostConstruct
 	public void ignoreSSLCertificateVerification() {
 		SSLContext ctx = null;
-        TrustManager[] trustAllCerts = new X509TrustManager[]{new X509TrustManager(){
+        TrustManager[] trustAllCerts = new X509TrustManager[]{new X509TrustManager() {
             public X509Certificate[] getAcceptedIssuers(){return null;}
             
-            // Do nothing
-            public void checkClientTrusted(X509Certificate[] certs, String authType){}
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            	// Do nothing
+            }
             
-            // Do nothing
-            public void checkServerTrusted(X509Certificate[] certs, String authType){}
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                // Do nothing	
+            }
         }};
         
         try {
