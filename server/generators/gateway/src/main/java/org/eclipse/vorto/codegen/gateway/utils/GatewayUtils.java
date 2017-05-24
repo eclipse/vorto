b4a249106207cc8fcd71812bb721ec0f3863a0c6
Erle Czar Mantos <ErleCzar.Mantos@bosch-si.com>
2017-05-24 10:35:46 +0800

    Features/gateway improvements (#565)
    * - Use only one environment config while will then be populated depending
      on passed configuration file.
    - Remove private server URLs in the configuration file.
    
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>
    
    * Allow external modification of the properties of embedded generators.
    
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>
    
    * Typo in String.format
    
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>
    
    * Remove unused import
    
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/utils/GatewayUtils.java
+++ server/generators/gateway/src/main/java/org/eclipse/vorto/codegen/gateway/utils/GatewayUtils.java
@@ -5,9 +5,11 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,7 +17,6 @@ import org.eclipse.vorto.codegen.api.GeneratorServiceInfo;
 import org.eclipse.vorto.codegen.api.IVortoCodeGenerator;
 import org.eclipse.vorto.codegen.api.ServiceClassifier;
 import org.eclipse.vorto.codegen.gateway.exception.NotFoundException;
-import org.eclipse.vorto.codegen.gateway.exception.PropertyLoadingException;
 import org.eclipse.vorto.codegen.gateway.model.Generator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,14 +28,20 @@ import org.springframework.util.StreamUtils;
 
 public class GatewayUtils {
 	
-	private static final String VORTO_SERVICE_TAGS = "vorto.service.tags";
-	private static final String VORTO_SERVICE_CLASSIFIER = "vorto.service.classifier";
-	private static final String VORTO_SERVICE_NAME = "vorto.service.name";
-	private static final String VORTO_SERVICE_IMAGE32X32 = "vorto.service.image32x32";
-	private static final String VORTO_SERVICE_IMAGE144X144 = "vorto.service.image144x144";
-	private static final String VORTO_SERVICE_DOCUMENTATION_URL = "vorto.service.documentationUrl";
-	private static final String VORTO_SERVICE_DESCRIPTION = "vorto.service.description";
-	private static final String VORTO_SERVICE_CREATOR = "vorto.service.creator";
+	private static final String FILE_PROPERTY_PREFIX = "vorto.service.%s";
+	private static final String ENV_PROPERTY_PREFIX = "vorto.service.%s.%s";
+	
+	private static final String[] PROPERTIES = {"tags", "classifier", "name", "image32x32", "image144x144", 
+			"documentationUrl", "description", "creator"};
+	
+	private static final String VORTO_SERVICE_TAGS = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[0]);
+	private static final String VORTO_SERVICE_CLASSIFIER = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[1]);
+	private static final String VORTO_SERVICE_NAME = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[2]);
+	private static final String VORTO_SERVICE_IMAGE32X32 = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[3]);
+	private static final String VORTO_SERVICE_IMAGE144X144 = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[4]);
+	private static final String VORTO_SERVICE_DOCUMENTATION_URL = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[5]);
+	private static final String VORTO_SERVICE_DESCRIPTION = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[6]);
+	private static final String VORTO_SERVICE_CREATOR = String.format(FILE_PROPERTY_PREFIX, PROPERTIES[7]);
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayUtils.class);
 	
@@ -42,27 +49,32 @@ public class GatewayUtils {
 		Objects.requireNonNull(configFile);
 		Objects.requireNonNull(generator);
 		
+		Properties properties = getProperties(configFile).orElseThrow(notFound(String.format("ConfigFile [%s]", configFile)));
+		
 		GeneratorServiceInfo serviceInfo = new GeneratorServiceInfo();
-		try {
-			Properties properties = new Properties();
-			properties.load(new ClassPathResource(configFile).getInputStream());
-			
-			serviceInfo.setCreator(properties.getProperty(VORTO_SERVICE_CREATOR));
-			serviceInfo.setDescription(properties.getProperty(VORTO_SERVICE_DESCRIPTION));
-			serviceInfo.setDocumentationUrl(properties.getProperty(VORTO_SERVICE_DOCUMENTATION_URL));
-			serviceInfo.setImage144x144(encodeToBase64(properties.getProperty(VORTO_SERVICE_IMAGE144X144)));
-			serviceInfo.setImage32x32(encodeToBase64(properties.getProperty(VORTO_SERVICE_IMAGE32X32)));
-			serviceInfo.setKey(generator.getServiceKey());
-			serviceInfo.setName(properties.getProperty(VORTO_SERVICE_NAME));
-			serviceInfo.setClassifier(ServiceClassifier.valueOf(properties.getProperty(VORTO_SERVICE_CLASSIFIER)));
-			serviceInfo.setTags(properties.getProperty(VORTO_SERVICE_TAGS,"").split(","));
-			
-		} catch (IOException e) {
-			throw new PropertyLoadingException("Error in loading configFile to Properties", e);
-		}
+		
+		serviceInfo.setCreator(properties.getProperty(VORTO_SERVICE_CREATOR));
+		serviceInfo.setDescription(properties.getProperty(VORTO_SERVICE_DESCRIPTION));
+		serviceInfo.setDocumentationUrl(properties.getProperty(VORTO_SERVICE_DOCUMENTATION_URL));
+		serviceInfo.setImage144x144(encodeToBase64(properties.getProperty(VORTO_SERVICE_IMAGE144X144)));
+		serviceInfo.setImage32x32(encodeToBase64(properties.getProperty(VORTO_SERVICE_IMAGE32X32)));
+		serviceInfo.setKey(generator.getServiceKey());
+		serviceInfo.setName(properties.getProperty(VORTO_SERVICE_NAME));
+		serviceInfo.setClassifier(ServiceClassifier.valueOf(properties.getProperty(VORTO_SERVICE_CLASSIFIER)));
+		serviceInfo.setTags(properties.getProperty(VORTO_SERVICE_TAGS,"").split(","));
 		
 		return serviceInfo;
 	};
+	
+	private static Optional<Properties> getProperties(String configFile) {
+		try {
+			Properties properties = new Properties();
+			properties.load(new ClassPathResource(configFile).getInputStream());
+			return Optional.of(properties);
+		} catch (IOException e) {
+			return Optional.empty();
+		}
+	}
 	
 	public static String encodeToBase64(String filename) {
 		InputStream ioStream = null;
@@ -97,16 +109,31 @@ public class GatewayUtils {
 	
 	public static Consumer<Generator> checkEnvModifications(Environment env) {
 		return gen -> {
-			String img32ModificationProperty = String.format("vorto.service.%s.image32x32", gen.getInstance().getServiceKey());
-			if (env.containsProperty(img32ModificationProperty)) {
-				LOGGER.info(String.format("Overriding 32x32 image for %s with %s", gen.getInstance().getServiceKey(), env.getProperty(img32ModificationProperty)));
-				gen.getInfo().setImage32x32(encodeToBase64(env.getProperty(img32ModificationProperty)));
-			}
-			String img144ModificationProperty = String.format("vorto.service.%s.image144x144", gen.getInstance().getServiceKey());
-			if (env.containsProperty(img144ModificationProperty)) {
-				LOGGER.info(String.format("Overriding 144x144 image for %s with %s", gen.getInstance().getServiceKey(), env.getProperty(img144ModificationProperty)));
-				gen.getInfo().setImage144x144(encodeToBase64(env.getProperty(img144ModificationProperty)));
-			}
+			Stream.of(PROPERTIES).forEach(property -> {
+				String propertyKey = gen.getInstance().getServiceKey();
+				String modProperty = String.format(ENV_PROPERTY_PREFIX, propertyKey, property);
+				if (env.containsProperty(modProperty)) {
+					String modValue = env.getProperty(modProperty);
+					LOGGER.info(String.format("Overriding %s for %s with %s", property, propertyKey, modValue));
+					if (PROPERTIES[0].equals(property)) { 
+						gen.getInfo().setTags(modValue.split(","));
+					} else if (PROPERTIES[1].equals(property)) {
+						gen.getInfo().setClassifier(ServiceClassifier.valueOf(modValue));
+					} else if (PROPERTIES[2].equals(property)) {
+						gen.getInfo().setName(modValue);
+					} else if (PROPERTIES[3].equals(property)) {
+						gen.getInfo().setImage32x32(encodeToBase64(modValue));
+					} else if (PROPERTIES[4].equals(property)) {
+						gen.getInfo().setImage144x144(encodeToBase64(modValue));
+					} else if (PROPERTIES[5].equals(property)) {
+						gen.getInfo().setDocumentationUrl(modValue);
+					} else if (PROPERTIES[6].equals(property)) {
+						gen.getInfo().setDescription(modValue);
+					} else if (PROPERTIES[7].equals(property)) {
+						gen.getInfo().setCreator(modValue);
+					}
+				}
+			});
 		};
 	}
 }
