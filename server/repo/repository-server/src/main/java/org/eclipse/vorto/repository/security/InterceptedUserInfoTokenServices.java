97583ebf1cca9aecaff097239161302162d99c10
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-05-25 18:10:18 +0800

    - Add support for Github oAuth provider - Allow enabling/disabling of oAuth providers - Github-authenticated users should go through registration screen
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

---
+++ server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/security/InterceptedUserInfoTokenServices.java
@@ -0,0 +1,42 @@
+package org.eclipse.vorto.repository.security;
+
+import java.util.Map;
+
+import org.eclipse.vorto.repository.service.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InterceptedUserInfoTokenServices extends UserInfoTokenServices {
+
+	@Autowired
+    private IUserRepository userRepository;
+	
+	@Autowired
+	public InterceptedUserInfoTokenServices(
+			@Value("${github.oauth2.resource.userInfoUri}") String userInfoEndpointUrl, 
+			@Value("${github.oauth2.client.clientId}") String clientId) {
+		super(userInfoEndpointUrl, clientId);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public OAuth2Authentication loadAuthentication(String accessToken)
+			throws AuthenticationException, InvalidTokenException {
+		OAuth2Authentication auth = super.loadAuthentication(accessToken);
+		Map<String, Object> userDetails = ((Map<String, Object>) auth.getUserAuthentication().getDetails());
+		
+		if (userRepository.findByEmail((String) userDetails.get("email")) == null) {
+			userDetails.put("isRegistered", "false");
+		} else {
+			userDetails.put("isRegistered", "true");
+		}
+		
+		return auth;
+	}
+}
