21c95f514e9ee9353ce4655217b67d5b8656aeaf
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-05-26 10:46:36 +0800

    Remove unused import.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/security/SecurityConfiguration.java
+++ server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/security/SecurityConfiguration.java
@@ -30,7 +30,6 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
