03da3d71bbc16afb10423fd0e7ed8612d54d347a
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-06-08 13:36:18 +0800

    Use bosch style in EIDP SSO login
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/security/eidp/EidpOAuth2RestTemplate.java
+++ server/repo/repository-server/src/main/java/org/eclipse/vorto/repository/security/eidp/EidpOAuth2RestTemplate.java
@@ -7,6 +7,10 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 public class EidpOAuth2RestTemplate extends OAuth2RestTemplate {
 
+	private static final String BOSCH_STYLE = "f8df8cf7-8ded-4434-84a5-0125e36fca0e";
+	private static final String STYLE_ID = "styleId";
+	private static final String ID_PROVIDER = "AD+AUTHORITY";
+	private static final String REDIRECT_TO_IDENTITY_PROVIDER = "RedirectToIdentityProvider";
 	private static final String RESOURCE2 = "resource";
 
 	public EidpOAuth2RestTemplate(EidpResourceDetails resource, OAuth2ClientContext context) {
@@ -20,6 +24,8 @@ public class EidpOAuth2RestTemplate extends OAuth2RestTemplate {
 			return super.acquireAccessToken(oauth2Context);
 		} catch(UserRedirectRequiredException e) {
 			e.getRequestParams().put(RESOURCE2, ((EidpResourceDetails) getResource()).getResource());
+			e.getRequestParams().put(STYLE_ID, BOSCH_STYLE);
+			e.getRequestParams().put(REDIRECT_TO_IDENTITY_PROVIDER, ID_PROVIDER);
 			throw e;
 		}
 	}
