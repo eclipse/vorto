5d228d9646ab827aad418ead7627888831b37e3f
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2017-07-05 15:42:29 +0800

    Fixes #682 - CLI error when sharing.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- cli/models/response.go
+++ cli/models/response.go
@@ -4,7 +4,12 @@ package vorto
 	Response - represents information after an POST /rest/model/ request, to validate and upload an model
  */
 type Response struct {
-	ErrorMessage	string 			`json:"errorMessage"`
-	HandleId  		string 			`json:"handleId"`
-	Valid			bool			`json:"valid"`
+	ErrorMessage	string 			      `json:"message"`
+	Result          []UploadMessageResult `json:"obj"`
+}
+
+type UploadMessageResult struct {
+	HandleId        string          `json:"handleId"`
+	Valid           bool            `json:"valid"`
+	ErrorMessage    string          `json:"errorMessage"`
 }
