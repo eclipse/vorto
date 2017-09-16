ff7ab9361552de5136e62a5fade07a1737e07e38
Oliver Meili <omi@ieee.org>
2017-09-16 20:23:12 +0800

    - Added copyright notice - Removed unused import
    Signed-off-by: Oliver Meili <omi@ieee.org>

--- server/repo/repository-mapping/src/main/java/org/eclipse/vorto/service/mapping/BleGattInitSequenceElement.java
+++ server/repo/repository-mapping/src/main/java/org/eclipse/vorto/service/mapping/BleGattInitSequenceElement.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2017 Oliver Meili
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * - Oliver Meili <omi@ieee.org>
+ */
 package org.eclipse.vorto.service.mapping;
 
 import java.util.Arrays;
--- server/repo/repository-mapping/src/test/java/org/eclipse/vorto/service/mapping/BleGattMappingTest.java
+++ server/repo/repository-mapping/src/test/java/org/eclipse/vorto/service/mapping/BleGattMappingTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2017 Oliver Meili
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * - Oliver Meili <omi@ieee.org>
+ */
 package org.eclipse.vorto.service.mapping;
 
 import static org.junit.Assert.assertEquals;
@@ -7,7 +21,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 
 import org.apache.commons.jxpath.Functions;
