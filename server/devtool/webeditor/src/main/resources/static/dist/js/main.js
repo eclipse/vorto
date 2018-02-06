af9e03b489ec009ce66941aa3ad56f7006e12761
vlvanchin <vanchinathan.lokanathan@bosch-si.com>
2018-02-06 16:34:53 +0800

    Fixes jquery file not found
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

--- server/devtool/webeditor/src/main/resources/static/dist/js/main.js
+++ server/devtool/webeditor/src/main/resources/static/dist/js/main.js
@@ -1,6 +1,6 @@
 require.config({
 	paths: {
-		"jquery" : "../../webjars/jquery/jquery.min",
+		"jquery" : "../../webjars/jquery/3.2.1/jquery.min",
 		"angular": "../../webjars/angularjs/1.3.13/angular.min",
 		"angular-route" : "../../webjars/angular-route/1.3.13/angular-route.min",
 		"angular-animate" : "../../webjars/angular-animate/1.3.13/angular-animate.min",
