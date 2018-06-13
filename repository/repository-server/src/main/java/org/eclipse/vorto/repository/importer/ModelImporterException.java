dd4bf1e5a8b1b7602c738404ce08582219ebae2b
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2018-06-13 16:16:16 +0800

    Add license headers.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-server/src/main/java/org/eclipse/vorto/repository/importer/ModelImporterException.java
+++ repository/repository-server/src/main/java/org/eclipse/vorto/repository/importer/ModelImporterException.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2015-2016 Bosch Software Innovations GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * Bosch Software Innovations GmbH - Please refer to git log
+ */
 package org.eclipse.vorto.repository.importer;
 
 public class ModelImporterException extends RuntimeException {
@@ -8,7 +22,6 @@ public class ModelImporterException extends RuntimeException {
 	private static final long serialVersionUID = -7641141207722584032L;
 
 	public ModelImporterException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ModelImporterException(String arg0) {
