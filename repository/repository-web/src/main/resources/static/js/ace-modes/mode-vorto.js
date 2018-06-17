28e7affcba8f816d687fa0e9742db887e2f44333
aedelmann <alexander.edelmann@bosch-si.com>
2018-06-17 18:57:43 +0800

    added fixes for codacy

--- repository/repository-web/src/main/resources/static/js/ace-modes/mode-vorto.js
+++ repository/repository-web/src/main/resources/static/js/ace-modes/mode-vorto.js
@@ -1,4 +1,4 @@
-ace.define('ace/mode/vorto', function(require, exports, module) {
+ace.define("ace/mode/vorto", function(require, exports, module) {
 
 var oop = require("ace/lib/oop");
 var TextMode = require("ace/mode/text").Mode;
@@ -10,13 +10,13 @@ var Mode = function() {
 oop.inherits(Mode, TextMode);
 
 (function() {
-    // Extra logic goes here. (see below)
+    
 }).call(Mode.prototype);
 
 exports.Mode = Mode;
 });
 
-ace.define('ace/mode/example_highlight_rules', function(require, exports, module) {
+ace.define("ace/mode/example_highlight_rules", function(require, exports, module) {
 
 var oop = require("ace/lib/oop");
 var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
@@ -41,7 +41,7 @@ var ExampleHighlightRules = function() {
 			]
 		};
     
-}
+};
 
 oop.inherits(ExampleHighlightRules, TextHighlightRules);
 
