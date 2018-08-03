68a08c19f97da289692266d2898874a9967cb70d
Alexander Edelmann <alexander.edelmann@bosch-si.com>
2018-08-03 18:12:21 +0800

    Fixed bug that invalid backup could destroy existing repo content

--- repository/repository-server/src/main/java/org/eclipse/vorto/repository/backup/impl/DefaultModelBackupService.java
+++ repository/repository-server/src/main/java/org/eclipse/vorto/repository/backup/impl/DefaultModelBackupService.java
@@ -16,19 +16,29 @@ package org.eclipse.vorto.repository.backup.impl;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.InvalidSerializedDataException;
 import javax.jcr.Item;
+import javax.jcr.ItemExistsException;
 import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.lock.LockException;
+import javax.jcr.nodetype.ConstraintViolationException;
+import javax.jcr.version.VersionException;
 
 import org.eclipse.vorto.repository.api.ModelInfo;
 import org.eclipse.vorto.repository.backup.IModelBackupService;
 import org.eclipse.vorto.repository.core.IModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Service
 public class DefaultModelBackupService implements IModelBackupService {
@@ -39,20 +49,47 @@ public class DefaultModelBackupService implements IModelBackupService {
 	@Autowired
 	private IModelRepository modelRepository;
 
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	public final String EXCEPTION_MESSAGE_CURRENT_CONTENT = "Not able to create a backup of the current content";
+	public final String EXCEPTION_MESSAGE_RESTORABLE_CONTENT = "Not able to restore the imported backup";
+
 	@Override
 	public byte[] backup() throws Exception {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		((org.modeshape.jcr.api.Session) session).exportDocumentView("/", baos, false, false);
 		baos.close();
 		return baos.toByteArray();
-
 	}
 
 	@Override
-	public void restore(byte[] backup) throws Exception {
+	public void restore(byte[] restorableContent) throws Exception {
+		byte[] currentContent = backup();
+
+		try {
+			LOGGER.info("attempting to import backup of current content");
+			doRestore(currentContent);
+		} catch (Exception invalidCurrentContent) {
+			throw new Exception(this.EXCEPTION_MESSAGE_CURRENT_CONTENT);
+		}
+
 		removeAll();
+
+		try {
+			LOGGER.info("attempting to import imported backup");
+			doRestore(restorableContent);
+		} catch (Exception invalidContent) {
+			doRestore(currentContent);
+			throw new Exception(this.EXCEPTION_MESSAGE_RESTORABLE_CONTENT);
+		}
+	}
+
+	private void doRestore(byte[] backup) throws AccessDeniedException, VersionException, PathNotFoundException,
+			ItemExistsException, ConstraintViolationException, InvalidSerializedDataException, LockException,
+			IOException, RepositoryException {
 		((org.modeshape.jcr.api.Session) session).getWorkspace().importXML("/", new ByteArrayInputStream(backup),
 				ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW);
+		LOGGER.info("created backup succesfully");
 	}
 
 	public Session getSession() {
@@ -76,8 +113,8 @@ public class DefaultModelBackupService implements IModelBackupService {
 		for (ModelInfo resource : this.modelRepository.search("*")) {
 			int namespaceIdx = resource.getId().getNamespace().indexOf(".");
 			if (namespaceIdx != -1) {
-				final String org = resource.getId().getNamespace().substring(0,namespaceIdx);
-		        rootNodes.add(org);
+				final String org = resource.getId().getNamespace().substring(0, namespaceIdx);
+				rootNodes.add(org);
 			} else {
 				rootNodes.add(resource.getId().getNamespace());
 			}
@@ -88,10 +125,9 @@ public class DefaultModelBackupService implements IModelBackupService {
 				Item item = session.getItem("/" + rootNode);
 				item.remove();
 			} catch (PathNotFoundException ex) {
+
 			}
 		}
-
 		this.session.save();
 	}
-
 }
--- repository/repository-server/src/test/java/org/eclipse/vorto/repository/backup/RepositoryAdminTest.java
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/backup/RepositoryAdminTest.java
@@ -6,15 +6,15 @@ import static org.junit.Assert.assertNotNull;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.vorto.repository.AbstractIntegrationTest;
 import org.eclipse.vorto.repository.backup.impl.DefaultModelBackupService;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.core.io.ClassPathResource;
 
 public class RepositoryAdminTest extends AbstractIntegrationTest {
 
 	private DefaultModelBackupService repositoryManager = null;
-	
-	
+
 	@Override
 	public void beforeEach() throws Exception {
 		super.beforeEach();
@@ -22,31 +22,60 @@ public class RepositoryAdminTest extends AbstractIntegrationTest {
 		repositoryManager.setModelRepository(this.modelRepository);
 		repositoryManager.setSession(jcrSession());
 	}
-	
+
 	@Test
 	public void testBackupFilesNoImages() throws Exception {
 		importModel("Color.type");
 		importModel("Colorlight.fbmodel");
 		importModel("Switcher.fbmodel");
-		importModel("HueLightStrips.infomodel");	
+		importModel("HueLightStrips.infomodel");
 		byte[] backedUpContent = repositoryManager.backup();
 		assertNotNull(backedUpContent);
 	}
-	
+
 	@Test
 	public void testRestoreBackup1() throws Exception {
-		this.repositoryManager.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
-		assertEquals(4,this.modelRepository.search("*").size());
+		this.repositoryManager
+				.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
+		assertEquals(4, this.modelRepository.search("*").size());
 	}
-	
+
 	@Test
-	@Ignore
 	public void testRestoreBackupExistingData() throws Exception {
-		this.repositoryManager.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
-		assertEquals(4,this.modelRepository.search("*").size());
-		this.repositoryManager.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
-		assertEquals(4,this.modelRepository.search("*").size());
+		this.repositoryManager
+				.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
+		assertEquals(4, this.modelRepository.search("*").size());
+		this.repositoryManager
+				.restore(IOUtils.toByteArray(new ClassPathResource("sample_models/backup1.xml").getInputStream()));
+		assertEquals(4, this.modelRepository.search("*").size());
+	}
+
+	@Test
+	public void testRestoreCorruptBackup() throws Exception {
+		this.repositoryManager.restore(
+				IOUtils.toByteArray(new ClassPathResource("sample_models/vortobackup_valid.xml").getInputStream()));
+		assertEquals(5, this.modelRepository.search("*").size());
+
+		try {
+			this.repositoryManager.restore(IOUtils
+					.toByteArray(new ClassPathResource("sample_models/vortobackup_corrupt.xml").getInputStream()));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		assertEquals(5, this.modelRepository.search("*").size());
 		System.out.println(this.modelRepository.search("*").get(0).getId());
 	}
-	
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testExceptionWhenImportingCorruptBackup() throws Exception {
+		thrown.expect(Exception.class);
+		thrown.expectMessage(this.repositoryManager.EXCEPTION_MESSAGE_RESTORABLE_CONTENT);
+		this.repositoryManager.restore(
+				IOUtils.toByteArray(new ClassPathResource("sample_models/vortobackup_corrupt.xml").getInputStream()));
+	}
+
 }
