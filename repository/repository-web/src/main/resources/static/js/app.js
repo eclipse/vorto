d7d6fe5857a38897c25a90049573e6d557c9443a
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2018-08-06 11:33:29 +0800

    Remove codacy errors
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-web/src/main/resources/static/js/app.js
+++ repository/repository-web/src/main/resources/static/js/app.js
@@ -131,7 +131,7 @@ repository.config([ "$routeProvider", "$httpProvider", function($routeProvider, 
                 if (user.isRegistered === "false") {
                     $location.path("/signup");
                 } else if(user.needUpdate === "true") {
-                	$location.path("/update");
+                    $location.path("/update");
                 }
             } else {
                 if ($rootScope.needAuthentication()) {
