da1207c92a33e9ea2a7450c990f09b5af04e7c5f
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2018-08-06 18:24:26 +0800

    1. Add user upgrade task for unhashing hashed authors in the model 2. Incorporated suggestions from Alex regarding UI
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-web/src/main/resources/static/js/controllers/updateController.js
+++ repository/repository-web/src/main/resources/static/js/controllers/updateController.js
@@ -3,14 +3,19 @@ repositoryControllers.controller("UpdateController", [ "$location", "$rootScope"
 	function ($location, $rootScope, $scope, $http, $routeParams) {
         $scope.acceptTerms = false;
         $scope.isUpdating = false;
+        $scope.accountUpdated = false;
+
+        $scope.doClose = function() {
+            $rootScope.init();
+            $location.path("/");
+        }
 
         $scope.doUpdate = function() {
             $scope.isUpdating = true;
 
             var updateSuccess = function(data, status, headers, config) {
                 $scope.isUpdating = false;
-                $rootScope.init();
-                $location.path("/");
+                $scope.accountUpdated = true;
             };
 
             var updateError = function(data, status, headers, config) {
