bb1d906110b9c1c821c6379bee2aeb4d233c3a3b
aedelmann <alexander.edelmann@bosch-si.com>
2018-10-28 19:20:32 +0800

    simplified complexity of equals in functionblock data

--- model/src/main/java/org/eclipse/vorto/model/AbstractProperty.java
+++ model/src/main/java/org/eclipse/vorto/model/AbstractProperty.java
@@ -93,26 +93,12 @@ public class AbstractProperty extends DefaultMappedElement {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
 		AbstractProperty other = (AbstractProperty) obj;
-		if (mandatory != other.mandatory)
+		if (mandatory == other.mandatory && name.equals(other.name) && type.equals(other.type))
+			return true;
+		else {
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
+		}
 	}
 
 
