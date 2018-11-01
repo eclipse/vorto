5a230f260c8c50d40db571c38fd507a14c8772e7
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2018-11-01 17:37:16 +0800

    Add equals so we can compare FileContent objects
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/core/FileContent.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/core/FileContent.java
@@ -30,7 +30,33 @@ public class FileContent {
 	public long getSize() {
 		return size;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fileName == null) ? 0 : fileName.hashCode());
+		result = prime * result + (int) (size ^ (size >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FileContent other = (FileContent) obj;
+		if (fileName == null) {
+			if (other.fileName != null)
+				return false;
+		} else if (!fileName.equals(other.fileName))
+			return false;
+		if (size != other.size)
+			return false;
+		return true;
+	}
 
 }
