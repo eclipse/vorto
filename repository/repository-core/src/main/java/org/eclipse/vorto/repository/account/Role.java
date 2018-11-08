dfa8cee8f0f29a0982dc44b7cdf8c8147a67bd2b
Fritz Otlinghaus <scriptkiddi@fap-studios.de>
2018-11-08 15:33:19 +0800

    Update Role.java

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/account/Role.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/account/Role.java
@@ -18,9 +18,9 @@ package org.eclipse.vorto.repository.account;
  * @author Alexander Edelmann - Robert Bosch (SEA) Pte. Ltd.
  */
 public enum Role {
-	USER, // minimal role for using the repository. It is restricted to only read access
+	USER, // minimal role for using the repository. It is restricted to only read access, but can generate things and write comments
 	ADMIN, // may execute admin functionality
-	MODEL_CREATOR, // users with this role may create models in the system
-	MODEL_PROMOTER, // users with this role may release models
+	MODEL_CREATOR, // users with this role may create models in the system, import models, edit his own models and delete own models as long as they are not released
+	MODEL_PROMOTER, // users with this role may start a release process for a model and deprecate a model
 	MODEL_REVIEWER // user with this role may review models and either approve or reject them
 }
