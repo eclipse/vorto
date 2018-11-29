16e97719161600dfd476821de370af6151dedb75
Schmidt-Dumont Georg (BCI/ESW24) <georg.schmidt-dumont@de.bosch.com>
2018-11-29 09:12:31 +0100

    [1199] Update Copyright Headers
    Signed-off-by: Schmidt-Dumont Georg (BCI/ESW24) <georg.schmidt-dumont@de.bosch.com>

--- repository/repository-core/src/test/java/org/eclipse/vorto/repository/actuator/GeneratorsHealthCheckTest.java
+++ repository/repository-core/src/test/java/org/eclipse/vorto/repository/actuator/GeneratorsHealthCheckTest.java
@@ -1,5 +1,12 @@
-/*
- * Copyright (c) 2018 Bosch Software Innovations GmbH. All rights reserved.
+/**
+ * Copyright (c) 2015-2018 Bosch Software Innovations GmbH and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of the Eclipse Public
+ * License v1.0 and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html The Eclipse
+ * Distribution License is available at http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors: Bosch Software Innovations GmbH - Please refer to git log
  */
 
 package org.eclipse.vorto.repository.actuator;
@@ -36,7 +43,7 @@ public class GeneratorsHealthCheckTest {
          .mock( ResponseEntity.class );
 
    @Before
-   public void setup() {
+   public void setUp() {
       Mockito.when( generatorInfoResponseEntitySuccess.getStatusCode() ).thenReturn( HttpStatus.OK );
       Mockito.when( generatorInfoResponseEntityFailure.getStatusCode() ).thenReturn( HttpStatus.NOT_FOUND );
    }
