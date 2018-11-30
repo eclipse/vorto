a574480e79bb31f007405e83a9734ec667c8981f
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2018-11-30 12:24:44 +0800

    Fixes #1172 - Model Instance data Validation fails at Integer properties
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/core/PayloadMappingController.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/core/PayloadMappingController.java
@@ -50,17 +50,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/rest/{tenant}/mappings")
 public class PayloadMappingController extends AbstractRepositoryController {
-
-  private static Gson gson = new GsonBuilder().create();
-
 
   @Autowired
   private ModelController modelController;
@@ -210,7 +205,7 @@ public class PayloadMappingController extends AbstractRepositoryController {
     MappingEngine engine = MappingEngine.create(testRequest.getSpecification());
 
     InfomodelValue mappedOutput =
-        engine.mapSource(gson.fromJson(testRequest.getSourceJson(), Object.class));
+        engine.mapSource(new ObjectMapper().readValue(testRequest.getSourceJson(), Object.class));
 
     TestMappingResponse response = new TestMappingResponse();
     response.setMappedOutput(new ObjectMapper().writeValueAsString(mappedOutput.serialize()));
