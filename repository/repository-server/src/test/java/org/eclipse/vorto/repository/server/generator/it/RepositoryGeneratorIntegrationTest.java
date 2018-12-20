a217dbf2e0337d46527f8055dde1ad2712ccf627
Fritz Otlinghaus <fritz@otlinghaus.it>
2018-12-20 13:08:22 +0800

    Delete unused layout files
    The files are identical to the theme we are using so they do not have to
    be overwritten
    
    Signed-off-by: Fritz Otlinghaus <fritz@otlinghaus.it>

--- repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/generator/it/RepositoryGeneratorIntegrationTest.java
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/generator/it/RepositoryGeneratorIntegrationTest.java
@@ -12,6 +12,19 @@
  */
 package org.eclipse.vorto.repository.server.generator.it;
 
+import com.google.common.collect.Sets;
+import com.google.gson.reflect.TypeToken;
+import org.eclipse.vorto.codegen.api.GeneratorServiceInfo;
+import org.eclipse.vorto.repository.sso.SpringUserUtils;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
 import static org.eclipse.vorto.repository.account.Role.ADMIN;
 import static org.eclipse.vorto.repository.account.Role.MODEL_CREATOR;
 import static org.eclipse.vorto.repository.account.Role.USER;
@@ -19,86 +32,64 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.fail;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import java.util.Collection;
-import java.util.List;
-import org.eclipse.vorto.codegen.api.GeneratorServiceInfo;
-import org.eclipse.vorto.repository.sso.SpringUserUtils;
-import org.hamcrest.Matchers;
-import org.junit.Test;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
-import org.springframework.test.web.servlet.MvcResult;
-import com.google.common.collect.Sets;
-import com.google.gson.reflect.TypeToken;
 
 
 public class RepositoryGeneratorIntegrationTest extends AbstractGeneratorIntegrationTest {
 
-  @Test
-  public void testGetRegisteredGeneratorServices() {
-    try {
-      vortoMockMvc.perform(get("/api/v1/generators").with(userAdmin)).andExpect(status().isOk())
-          .andExpect(jsonPath("$", hasSize(getGenerators().size())));
-    } catch (Exception e) {
-      fail("failed because of Exception: " + e.toString());
+    @Test public void testGetRegisteredGeneratorServices() {
+        try {
+            vortoMockMvc.perform(get("/api/v1/generators").with(userAdmin))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(getGenerators().size())));
+        } catch (Exception e) {
+            fail("failed because of Exception: " + e.toString());
+        }
     }
-  }
 
-  @Test
-  public void testGetGeneratorInfo() {
-    try {
-      for (GeneratorServiceInfo genInfo : getGenerators()) {
-        System.out.println("Checking for [" + genInfo.getKey() + "]");
-        vortoMockMvc.perform(get("/api/v1/generators/" + genInfo.getKey()))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.name", Matchers.is(genInfo.getName())))
-            .andExpect(jsonPath("$.description", Matchers.is(genInfo.getDescription())));
-      }
-    } catch (Exception e) {
-      fail("failed because of Exception: " + e.toString());
+    @Test public void testGetGeneratorInfo() {
+        try {
+            for (GeneratorServiceInfo genInfo : getGenerators()) {
+                System.out.println("Checking for [" + genInfo.getKey() + "]");
+                vortoMockMvc.perform(get("/api/v1/generators/" + genInfo.getKey()))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", Matchers.is(genInfo.getName())))
+                    .andExpect(jsonPath("$.description", Matchers.is(genInfo.getDescription())));
+            }
+        } catch (Exception e) {
+            fail("failed because of Exception: " + e.toString());
+        }
     }
-  }
 
-  @Test
-  public void testGenerate() {
-    try {
-      SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor user =
-          user("admin").password("pass").authorities(
-              SpringUserUtils.toAuthorityList(Sets.newHashSet(ADMIN, MODEL_CREATOR, USER)));
+    @Test public void testGenerate() {
+        try {
+            SecurityMockMvcRequestPostProcessors.UserRequestPostProcessor user =
+                user("admin").password("pass").authorities(
+                    SpringUserUtils.toAuthorityList(Sets.newHashSet(ADMIN, MODEL_CREATOR, USER)));
 
-      vortoMockMvc
-          .perform(put("/rest/default/models/com.test:Location:1.0.0").with(user)
-              .contentType(MediaType.APPLICATION_JSON)
-              .content(loadModel("Functionblock", "Location.fbmodel")))
-          .andExpect(status().isOk());
+            createModel("Location.fbmodel", "com.test:Location:1.0.0");
+            createModel("TrackingDevice.infomodel", "com.test:TrackingDevice:1.0.0");
+            // Creating a model takes a couple seconds
+            sleep(5000);
 
-      vortoMockMvc
-          .perform(put("/rest/default/models/com.test:TrackingDevice:1.0.0").with(user)
-              .contentType(MediaType.APPLICATION_JSON)
-              .content(loadModel("InformationModel", "TrackingDevice.infomodel")))
-          .andExpect(status().isOk());
+            vortoMockMvc.perform(get("/api/v1/search/models?expression="))
+                .andExpect(status().isOk());
+            vortoMockMvc.perform(
+                get("/api/v1/generators/boschiotsuite/models/com.test:TrackingDevice:1.0.0?language=java")
+                    .with(user)).andExpect(status().isOk())
+                .andExpect(ZipFileCompare.equals(loadResource("generated-boschiotsuite.zip")));
 
-      vortoMockMvc.perform(get("/api/v1/search/models?expression="))
-          .andExpect(status().isOk());
-
-      vortoMockMvc
-          .perform(get("/api/v1/generators/boschiotsuite/models/com.test:TrackingDevice:1.0.0?language=java"))
-          .andExpect(status().isOk())
-          .andExpect(ZipFileCompare.equals(loadResource("generated-boschiotsuite.zip")));
-        
-
-    } catch (Exception e) {
-      fail("failed because of Exception: " + e.toString());
+        } catch (Exception e) {
+            fail("failed because of Exception: " + e.toString());
+        }
     }
-  }
 
-  private Collection<GeneratorServiceInfo> getGenerators() throws Exception {
-    MvcResult result = generatorMockMvc.perform(get("/rest/generators")).andReturn();
-    return gson.fromJson(new String(result.getResponse().getContentAsByteArray()),
-        new TypeToken<List<GeneratorServiceInfo>>() {}.getType());
-  }
+    private Collection<GeneratorServiceInfo> getGenerators() throws Exception {
+        MvcResult result = generatorMockMvc.perform(get("/rest/generators")).andReturn();
+        return gson.fromJson(new String(result.getResponse().getContentAsByteArray()),
+            new TypeToken<List<GeneratorServiceInfo>>() {
+            }.getType());
+    }
 
 }
