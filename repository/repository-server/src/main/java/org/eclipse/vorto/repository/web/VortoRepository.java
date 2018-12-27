48fb1483ae046029e8a30d4c49dac4c7b2e77789
aedelmann <alexander.edelmann@bosch-si.com>
2018-12-28 06:57:17 +0800

    fixed license headers

--- repository/repository-server/src/main/java/org/eclipse/vorto/repository/web/VortoRepository.java
+++ repository/repository-server/src/main/java/org/eclipse/vorto/repository/web/VortoRepository.java
@@ -11,20 +11,6 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.vorto.repository.web;
-/**
- * Copyright (c) 2015-2016 Bosch Software Innovations GmbH and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * The Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- * Bosch Software Innovations GmbH - Please refer to git log
- */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -40,8 +26,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("org.eclipse.vorto.repository")
 @ComponentScan("org.eclipse.vorto.repository")
 public class VortoRepository {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(VortoRepository.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(VortoRepository.class, args);
+  }
 }
