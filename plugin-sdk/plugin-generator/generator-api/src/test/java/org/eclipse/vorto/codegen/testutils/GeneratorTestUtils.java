c1caf3a4d40cc052d0d477996d921d6b6062b957
Alexander Edelmann <alexander.edelmann@bosch-si.com>
2019-04-20 14:38:32 +0800

    Feature/postman script (#1481)
    * first draft of bosch iot suite Provisioning script for postman
    
    * Fixes #1476
    
    * Fixes codecy issues

--- plugin-sdk/plugin-generator/generator-api/src/test/java/org/eclipse/vorto/codegen/testutils/GeneratorTestUtils.java
+++ plugin-sdk/plugin-generator/generator-api/src/test/java/org/eclipse/vorto/codegen/testutils/GeneratorTestUtils.java
@@ -14,17 +14,37 @@ package org.eclipse.vorto.codegen.testutils;
 
 import org.eclipse.vorto.core.api.model.datatype.DatatypeFactory;
 import org.eclipse.vorto.core.api.model.datatype.Entity;
+import org.eclipse.vorto.core.api.model.datatype.Enum;
+import org.eclipse.vorto.core.api.model.datatype.EnumLiteral;
+import org.eclipse.vorto.core.api.model.datatype.ObjectPropertyType;
 import org.eclipse.vorto.core.api.model.datatype.PrimitivePropertyType;
 import org.eclipse.vorto.core.api.model.datatype.PrimitiveType;
 import org.eclipse.vorto.core.api.model.datatype.Property;
+import org.eclipse.vorto.core.api.model.datatype.Type;
 import org.eclipse.vorto.core.api.model.functionblock.Configuration;
 import org.eclipse.vorto.core.api.model.functionblock.FunctionBlock;
 import org.eclipse.vorto.core.api.model.functionblock.FunctionblockFactory;
 import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel;
 import org.eclipse.vorto.core.api.model.functionblock.Status;
+import org.eclipse.vorto.core.api.model.informationmodel.FunctionblockProperty;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModelFactory;
 import org.eclipse.vorto.core.api.model.model.ModelId;
 
 public abstract class GeneratorTestUtils {
+	
+	private static Property createProperty(String name, Type type) {
+		Property prop = DatatypeFactory.eINSTANCE.createProperty();
+		prop.setName(name);
+		prop.setType(createObjectType(type));
+		return prop;
+	} 
+	
+	private static ObjectPropertyType createObjectType(Type type) {
+		ObjectPropertyType typeObj = DatatypeFactory.eINSTANCE.createObjectPropertyType();
+		typeObj.setType(type);
+		return typeObj;
+	}
 
 	private static Property createProperty(String name, PrimitiveType type) {
 		Property prop = DatatypeFactory.eINSTANCE.createProperty();
@@ -41,6 +61,10 @@ public abstract class GeneratorTestUtils {
 		
 	public static FunctionblockBuilder newFunctionblock(ModelId modelId) {
 		return new FunctionblockBuilder().withId(modelId);
+	}
+	
+	public static InformationModelBuilder newInformationModel(ModelId modelId) {
+		return new InformationModelBuilder().withId(modelId);
 	}
 	
 	public static EntityBuilder newEntity(ModelId modelId) {
@@ -68,10 +92,77 @@ public abstract class GeneratorTestUtils {
 			return this;
 		}
 		
+		public EntityBuilder withProperty(String name, Type objectType) {
+			this.entity.getProperties().add(GeneratorTestUtils.createProperty(name, objectType));
+			return this;
+		}
+		
 		public Entity build() {
 			return this.entity;
 		}
 		
+	}
+	
+	public static EnumBuilder newEnum(ModelId modelId) {
+		return new EnumBuilder().withId(modelId);
+	}
+	
+	public static class EnumBuilder {
+		private Enum enumeration;
+		
+		public EnumBuilder() {
+			this.enumeration = DatatypeFactory.eINSTANCE.createEnum();
+		}
+		
+		public EnumBuilder withId(ModelId id) {
+			enumeration.setName(id.getName());
+			enumeration.setDisplayname(id.getName());
+			enumeration.setNamespace(id.getNamespace());
+			enumeration.setVersion(id.getVersion());
+			return this;
+		}
+		
+		public EnumBuilder withLiterals(String... values) {
+			for (String value : values) {
+				EnumLiteral literal = DatatypeFactory.eINSTANCE.createEnumLiteral();
+				literal.setName(value);
+				this.enumeration.getEnums().add(literal);
+			}
+			return this;
+		}
+		
+		public Enum build() {
+			return this.enumeration;
+		}
+	}
+	
+	public static class InformationModelBuilder {
+		
+		private InformationModel infomodel;
+		
+		public InformationModelBuilder() {
+			this.infomodel = InformationModelFactory.eINSTANCE.createInformationModel();
+		}
+		
+		public InformationModelBuilder withId(ModelId id) {
+			infomodel.setName(id.getName());
+			infomodel.setDisplayname(id.getName());
+			infomodel.setNamespace(id.getNamespace());
+			infomodel.setVersion(id.getVersion());
+			return this;
+		}
+		
+		public InformationModelBuilder withFunctionBlock(FunctionblockModel fbm, String propertyName) {
+			FunctionblockProperty property = InformationModelFactory.eINSTANCE.createFunctionblockProperty();
+			property.setName(propertyName);
+			property.setType(fbm);
+			this.infomodel.getProperties().add(property);
+			return this;
+		}
+		
+		public InformationModel build() {
+			return this.infomodel;
+		}
 	}
 	
 	public static class FunctionblockBuilder {
@@ -103,7 +194,26 @@ public abstract class GeneratorTestUtils {
 			return this;
 		}
 		
+		public FunctionblockBuilder withStatusProperty(String name, Type type) {
+			if (this.fb.getStatus() == null) {
+				Status status = FunctionblockFactory.eINSTANCE.createStatus();
+				this.fb.setStatus(status);
+			}
+		
+			this.fb.getStatus().getProperties().add(GeneratorTestUtils.createProperty(name, type));
+			return this;
+		}
+		
 		public FunctionblockBuilder withConfiguration(String name, PrimitiveType type) {
+			if (this.fb.getConfiguration() == null) {
+				Configuration configuration = FunctionblockFactory.eINSTANCE.createConfiguration();				
+				this.fb.setConfiguration(configuration);
+			}
+			this.fb.getConfiguration().getProperties().add(GeneratorTestUtils.createProperty(name, type));
+			return this;
+		}
+		
+		public FunctionblockBuilder withConfiguration(String name, Type type) {
 			if (this.fb.getConfiguration() == null) {
 				Configuration configuration = FunctionblockFactory.eINSTANCE.createConfiguration();				
 				this.fb.setConfiguration(configuration);
