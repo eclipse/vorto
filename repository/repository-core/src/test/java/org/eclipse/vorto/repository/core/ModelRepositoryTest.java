389a02936f7d9593703607252af6353a44a64836
aedelmann <alexander.edelmann@bosch-si.com>
2019-04-21 22:54:13 +0800

    Fixes #1490

--- repository/repository-core/src/test/java/org/eclipse/vorto/repository/core/ModelRepositoryTest.java
+++ repository/repository-core/src/test/java/org/eclipse/vorto/repository/core/ModelRepositoryTest.java
@@ -175,27 +175,25 @@ public class ModelRepositoryTest extends AbstractIntegrationTest {
   }
 
   @Test
-  public void testSearchModelWithFilters1() {
+  public void testSearchModelByModelName() {
     importModel("Color.type");
     importModel("Colorlight.fbmodel");
     importModel("Switcher.fbmodel");
     importModel("HueLightStrips.infomodel");
     assertEquals(1, modelRepository.search("name:Color").size());
-    assertEquals(3, modelRepository.search("name:Color name:Switcher name:HueLightStrips").size());
   }
-
+  
   @Test
-  public void testSearchModelWithFilters2() {
+  public void testSearchModelByNameWildcard() {
     importModel("Color.type");
     importModel("Colorlight.fbmodel");
     importModel("Switcher.fbmodel");
     importModel("HueLightStrips.infomodel");
-    assertEquals(1, modelRepository.search("name:Color version:1.0.0   ").size());
-    assertEquals(0, modelRepository.search("name:Color version:1.0.1").size());
+    assertEquals(2, modelRepository.search("name:Color*").size());
   }
 
   @Test
-  public void testSearchModelWithFilters3() {
+  public void testSearchModelByNamespace() {
     importModel("Color.type");
     importModel("Colorlight.fbmodel");
     importModel("Switcher.fbmodel");
@@ -206,9 +204,19 @@ public class ModelRepositoryTest extends AbstractIntegrationTest {
     assertEquals(2, modelRepository.search("namespace:com.mycompany   version:1.0.0").size());
 
   }
+  
+  @Test
+  public void testSearchModelByType() {
+    importModel("Color.type");
+    importModel("Colorlight.fbmodel");
+    importModel("Switcher.fbmodel");
+    importModel("ColorLightIM.infomodel");
+    importModel("HueLightStrips.infomodel");
+    assertEquals(2, modelRepository.search("Functionblock").size());
+  }
 
   @Test
-  public void testSearchModelWithFilters4() {
+  public void testSearchModelByNameAndType() {
     importModel("Color.type");
     importModel("Colorlight.fbmodel");
     importModel("Switcher.fbmodel");
@@ -216,7 +224,17 @@ public class ModelRepositoryTest extends AbstractIntegrationTest {
     importModel("HueLightStrips.infomodel");
     assertEquals(0, modelRepository.search("name:Switcher InformationModel").size());
     assertEquals(1, modelRepository.search("name:Switcher Functionblock").size());
-    assertEquals(2, modelRepository.search("Functionblock").size());
+  }
+  
+  @Test
+  public void testSearchModelWithFilters5() {
+    importModel("Color.type");
+    importModel("Colorlight.fbmodel");
+    importModel("Switcher.fbmodel");
+    importModel("ColorLightIM.infomodel");
+    importModel("HueLightStrips.infomodel");
+    assertEquals(1, modelRepository.search("Functionblock name:Color*").size());
+
   }
 
   @Test
