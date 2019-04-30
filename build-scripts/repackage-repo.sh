6c12eebd0724c7e4de2abec9f7428b9fee4072e2
vlvanchin <vanchinathan.lokanathan@bosch-si.com>
2019-04-30 08:08:40 +0800

    moving script code to script file
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

--- build-scripts/repackage-repo.sh
+++ build-scripts/repackage-repo.sh
@@ -2,10 +2,29 @@
 #
 # this script is to repackage repository jar with neccessory jars
 #
-echo "downloading mariadb using curl"
-curl -o curlDownload/mariadb-connector-java-2.3.0.jar https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar
+# copying the mariadb java client
+mkdir ~/wgetDownload
+wget -P ~/wgetDownload/ https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar
 
-echo "downloading mariadb using wget"
-wget -P wgetDownload/ https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar
+# setting up artifact uploads to aws
+mkdir -p ~/aws-upload/tmp
 
-exit 1
+# copying the repository artifact to the aws-upload
+cp ./repository/repository-server/target/infomodelrepository.jar ~/aws-upload/tmp
+cd ~/aws-upload/tmp
+jar -xvf infomodelrepository.jar
+rm -f infomodelrepository.jar
+cp ~/wgetDownload/mariadb-java-client-2.3.0.jar ./BOOT-INF/lib/
+pwd
+jar -cvf infomodelrepository-dbclient.jar .
+cd ~
+cp ~/aws-upload/tmp/infomodelrepository-dbclient.jar ~/aws-upload/${ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
+rm -rf ~/aws-upload/tmp
+ls -l
+
+# copying the official generators to the aws-upload folder
+cp ~/generators/generator-runner/target/generator-runner-exec.jar ~/aws-upload/${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
+
+# list the contents of aws-upload folder
+ls -al ~/aws-upload
+
