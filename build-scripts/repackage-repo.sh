5b50078a3a1e061098c3c45573d26469f7ac1628
vlvanchin <vanchinathan.lokanathan@bosch-si.com>
2019-04-30 09:02:09 +0800

    removed temporarly for speedup build and fixed relative paths in script
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

--- build-scripts/repackage-repo.sh
+++ build-scripts/repackage-repo.sh
@@ -2,28 +2,32 @@
 #
 # this script is to repackage repository jar with neccessory jars
 #
-# copying the mariadb java client
 pwd
-exit 1
 
-mkdir ~/wgetDownload
-wget -P ~/wgetDownload/ https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar
+# copying the mariadb java client
+mkdir ./wgetDownload
+wget -P ./wgetDownload/ https://downloads.mariadb.com/Connectors/java/connector-java-2.3.0/mariadb-java-client-2.3.0.jar
 
 # setting up artifact uploads to aws
-mkdir -p ~/aws-upload/tmp
+mkdir -p ./aws-upload/{tmp,repo,gen}
+
+# copying the repo and gen to respective folders
+cp ./repository/repository-server/target/infomodelrepository.jar ./aws-upload/repo
+cp ./generators/generator-runner/target/generator-runner-exec.jar ./aws-upload/gen
 
 # copying the repository artifact to the aws-upload
-cp ./repository/repository-server/target/infomodelrepository.jar ~/aws-upload/tmp
-cd ~/aws-upload/tmp
-jar -xvf infomodelrepository.jar
+cp ./aws-upload/repo/infomodelrepository.jar ./aws-upload/tmp
+cd aws-upload/tmp
+jar -xf infomodelrepository.jar
 rm -f infomodelrepository.jar
-cp ~/wgetDownload/mariadb-java-client-2.3.0.jar ./BOOT-INF/lib/
-pwd
-jar -cvf infomodelrepository-dbclient.jar .
-cd ~
-cp ~/aws-upload/tmp/infomodelrepository-dbclient.jar ~/aws-upload/${ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
-rm -rf ~/aws-upload/tmp
-ls -l
+cp ../../wgetDownload/mariadb-java-client-2.3.0.jar ./BOOT-INF/lib/
+jar -cf infomodelrepository-dbclient.jar .
+cd ../..
+cp ./aws-upload/tmp/infomodelrepository-dbclient.jar ~/aws-upload/${ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
+rm -rf ./aws-upload/tmp/*
+ls -l ./aws-upload
+
+exit 0
 
 # copying the official generators to the aws-upload folder
 cp ~/generators/generator-runner/target/generator-runner-exec.jar ~/aws-upload/${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
