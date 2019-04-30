9595760fee7ba4a51d2b1f1cc2c64e7d2b132f52
vlvanchin <vanchinathan.lokanathan@bosch-si.com>
2019-04-30 09:19:52 +0800

    included echo statements
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

--- build-scripts/repackage-repo.sh
+++ build-scripts/repackage-repo.sh
@@ -12,22 +12,26 @@ wget -P ./wgetDownload/ https://downloads.mariadb.com/Connectors/java/connector-
 mkdir -p ./aws-upload/{tmp,repo,gen}
 
 # copying the repo and gen to respective folders
+echo "copying both repo and gen artifacts to specific folders"
 cp ./repository/repository-server/target/infomodelrepository.jar ./aws-upload/repo
 cp ./generators/generator-runner/target/generator-runner-exec.jar ./aws-upload/gen
 
 # copying the repository artifact to the aws-upload
+echo "copying the repo artifact to tmp"
 cp ./aws-upload/repo/infomodelrepository.jar ./aws-upload/tmp
 cd aws-upload/tmp
-jar -xf infomodelrepository.jar
+jar -xvf infomodelrepository.jar
 rm -f infomodelrepository.jar
 cp ../../wgetDownload/mariadb-java-client-2.3.0.jar ./BOOT-INF/lib/
-jar -cf infomodelrepository-dbclient.jar .
+jar -cvf infomodelrepository-dbclient.jar .
 cd ../..
-cp ./aws-upload/tmp/infomodelrepository-dbclient.jar ~/aws-upload/${ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
+echo "pwd : $(pwd)"
+ls -l
+cp ./aws-upload/tmp/infomodelrepository-dbclient.jar ./aws-upload/${ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
 rm -rf ./aws-upload/tmp/*
 ls -l ./aws-upload
 
-exit 0
+exit 1
 
 # copying the official generators to the aws-upload folder
 cp ~/generators/generator-runner/target/generator-runner-exec.jar ~/aws-upload/${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
