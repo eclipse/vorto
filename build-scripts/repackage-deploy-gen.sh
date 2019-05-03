d5e8260d26411ecf5a46704c6588bc37734a3cc9
Lokanathan Vanchinathan <vanchinathan.lokanathan@bosch-si.com>
2019-05-03 19:59:45 +0800

    Feature/issue 1436 integrate auto deployment using travisci (#1527)
    * configured the env vars and uploading artifacts to aws-upload folder
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * made sonar to do for dev and master just for testing
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fixed syntax error
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * installing awscli, and uploading repo and gen to s3
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * separate scripts in script provider
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fixed loss of artifacts from upload folder
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * corrected yml indentation
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added the placeholders for the keys
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fixing parsing error
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added version to offical generators
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added update environment for official generators
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * adding script to download java client
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added script for db java client download
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * add script to include mariadb java client to repo jar
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * add fix the path
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * fixed folder name
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * updated .travisci
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * copying generator runnuer jar
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * removed test for quicker testing.. will be added later
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * code reverted
    
    Signed-off-by: vlvanchin <vlvanchin@gmail.com>
    
    * moving script code to script file
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * to display working directory
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * removed testing for faster cycle, it will be replaced back later
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * removed temporarly for speedup build and fixed relative paths in script
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * included echo statements
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * updated image reference, scripts for gen added
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * refactored some script
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * using the same manifest.MF file
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * added mysql configurations to the int profile
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * jaring without compression
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * update the scrip to deploy to ELB
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * updating to check valid url, styles, do integration tests
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * removed send images to docker hub
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * deployment works for development branch and docker push script is removed
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>
    
    * substituting place holders for keys
    
    Signed-off-by: vlvanchin <vanchinathan.lokanathan@bosch-si.com>

---
+++ build-scripts/repackage-deploy-gen.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+#
+# this script is to repackage generator jar with neccessory images
+#
+pwd
+
+# copying the official generators to the aws-upload tmp folder
+echo "process to extract and include images from s3 img/official folder"
+cp ./aws-upload/gen/generator-runner-exec.jar ./aws-upload/tmp
+cd aws-upload/tmp
+jar -xvf generator-runner-exec.jar
+rm -f generator-runner-exec.jar
+aws s3 cp s3://$VORTO_S3_BUCKET/img/official ./BOOT-INF/classes/img --recursive
+jar -cvmf0 META-INF/MANIFEST.MF generator-runner-exec-withimg.jar .
+
+echo "copying the jar with images to aws-upload folder"
+cp generator-runner-exec-withimg.jar ../${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar
+cd ../..
+
+echo "pwd $(pwd)"
+ls -l ./aws-upload
+
+# uploading to s3
+echo "uploading to s3 bucket"
+aws s3 cp ./aws-upload/${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar s3://$VORTO_S3_BUCKET --acl "private" --storage-class "STANDARD_IA" --only-show-errors --no-guess-mime-type
+
+# updating the application-version
+echo "versioning the artifact in EBS"
+aws elasticbeanstalk create-application-version --application-name "VortoRepoServer" --no-auto-create-application --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_official_gen" --description "Build ${TRAVIS_JOB_NUMBER} - Git Revision ${TRAVIS_COMMIT_SHORT} for offical generators" --source-bundle S3Bucket="$VORTO_S3_BUCKET",S3Key="${GEN_ARTIFACT_NAME}_${ELASTIC_BEANSTALK_LABEL}.jar"
+
+# updating  environment
+echo "updating environment in EBS"
+aws elasticbeanstalk update-environment --application-name "VortoRepoServer" --environment-name "Vortogenerators-env-dev" --version-label "build-job_${ELASTIC_BEANSTALK_LABEL}_official_gen"
+
+echo "finished running repackage-deploy-gen.sh"
+
