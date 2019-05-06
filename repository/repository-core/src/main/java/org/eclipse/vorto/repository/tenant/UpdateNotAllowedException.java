99562576d063ea50c747bdf731081dda1b51d09c
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2019-05-06 11:32:23 +0800

    Changes to the Tenant controller. Add a way to request namespace.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

---
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/tenant/UpdateNotAllowedException.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.vorto.repository.tenant;
+
+public class UpdateNotAllowedException extends RuntimeException {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -2364334712897070077L;
+
+  public UpdateNotAllowedException(String username, String tenantId) {
+    super(String.format("User '%s' is not allowed to update tenant '%s'.", username, tenantId));
+  }
+}
