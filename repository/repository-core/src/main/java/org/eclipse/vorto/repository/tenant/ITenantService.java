e1d7cf06633bae1d87c0193bec721380a51a1bc8
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2019-05-06 15:36:19 +0800

    Document interface
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/tenant/ITenantService.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/tenant/ITenantService.java
@@ -18,26 +18,82 @@ import java.util.Set;
 import org.eclipse.vorto.repository.core.IUserContext;
 import org.eclipse.vorto.repository.domain.Tenant;
 
-// TODO : Document interface
 public interface ITenantService {
 
+  /**
+   * Check if a tenant with the tenantId exist
+   * @param tenantId the tenantId to check
+   * @return 
+   */
   boolean tenantExist(String tenantId);
 
+  /**
+   * Returns whether an existing namespace conflicts with a current namespace
+   * @param namespace to check
+   * @return
+   */
   boolean conflictsWithExistingNamespace(String namespace);
   
+  /**
+   * Create or update a tenant
+   * 
+   * @param tenantId the tenantId of this tenant
+   * @param defaultNamespace the default namespace of the tenant
+   * @param tenantAdmins a set of userIds who will be given tenant admin rights for this tenant
+   * @param namespaces a set of namespaces to be given to the tenant
+   * @param authenticationProvider the authentication provider of this tenant (currently unused)
+   * @param authorizationProvider the authorization provider for this tenant (currently unused)
+   * @param userContext the user who executed this call (he will become the tenant owner)
+   * @return the tenant
+   */
   Tenant createOrUpdateTenant(String tenantId, String defaultNamespace, Set<String> tenantAdmins,
       Optional<Set<String>> namespaces, Optional<String> authenticationProvider,
       Optional<String> authorizationProvider, IUserContext userContext);
 
+  /**
+   * Returns the tenant given the tenantId
+   * 
+   * @param tenantId
+   * @return
+   */
   Optional<Tenant> getTenant(String tenantId);
   
+  /**
+   * Returns the tenant who owns the namespace
+   * @param namespace
+   * @return
+   */
   Optional<Tenant> getTenantFromNamespace(String namespace);
   
+  /**
+   * Get all the tenants
+   * @return
+   */
   Collection<Tenant> getTenants();
 
+  /**
+   * Update the namespace for the tenant
+   * @param tenantId
+   * @param namespaces
+   * @return
+   */
   boolean updateTenantNamespaces(String tenantId, Set<String> namespaces);
 
+  /**
+   * Add namespaces to tenant
+   * 
+   * @param tenantId
+   * @param namespaces
+   * @return
+   */
   boolean addNamespacesToTenant(String tenantId, Set<String> namespaces);
   
+  /**
+   * Deletes the tenant
+   * 
+   * @param tenant
+   * @param userContext
+   * @return
+   */
   boolean deleteTenant(Tenant tenant, IUserContext userContext);
 }
