0df5f18f5be5e17e7556714974097d40eae0180b
Erle Czar Mantos <erleczar.mantos@bosch-si.com>
2019-06-20 16:31:09 +0800

    Add indexing service.
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/core/indexing/IndexingSupervisor.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/core/indexing/IndexingSupervisor.java
@@ -1,5 +1,6 @@
 package org.eclipse.vorto.repository.core.indexing;
 
+import org.eclipse.vorto.model.ModelId;
 import org.eclipse.vorto.repository.core.ModelInfo;
 import org.eclipse.vorto.repository.core.events.AppEvent;
 import org.eclipse.vorto.repository.core.events.EventType;
@@ -10,14 +11,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class IndexingSupervisor implements ApplicationListener<AppEvent>  {
   
-  @Autowired
   private IIndexingService indexingService;
   
+  public IndexingSupervisor(@Autowired IIndexingService indexingService) {
+    this.indexingService = indexingService;
+  }
+
   @Override
   public void onApplicationEvent(AppEvent event) {
     if (event.getEventType() == EventType.MODEL_CREATED) {
       ModelInfo modelInfo = (ModelInfo) event.getSubject();
-      indexingService.indexModel(modelInfo, event.getUserContext());
+      indexingService.indexModel(modelInfo, event.getUserContext().getTenant());
+    } else if (event.getEventType() == EventType.MODEL_UPDATED) {
+      ModelInfo modelInfo = (ModelInfo) event.getSubject();
+      indexingService.updateIndex(modelInfo);
+    } else if (event.getEventType() == EventType.MODEL_DELETED) {
+      ModelId modelId = (ModelId) event.getSubject();
+      indexingService.deleteIndex(modelId);
+    } else if (event.getEventType() == EventType.TENANT_DELETED) {
+      indexingService.deleteIndexForTenant(event.getUserContext().getTenant());
     }
   }
 
