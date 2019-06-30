de4f7fae2501a1a4c1361fc7c994416bdff653f3
aedelmann <alexander.edelmann@bosch-si.com>
2019-06-30 11:14:30 +0800

    Added AllTests for INT tests. Ignore alltest suites during maven build

---
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/web/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.vorto.repository.web;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({AccountControllerTest.class, ModelRepositoryControllerTest.class})
+public class AllTests {
+
+}
