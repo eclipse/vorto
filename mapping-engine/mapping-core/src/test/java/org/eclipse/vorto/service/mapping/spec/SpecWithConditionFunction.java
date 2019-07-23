82f4ab99677f8164c5844fa358e0b97f30d1bd73
Alexander Edelmann <alexander.edelmann@bosch-si.com>
2019-07-23 19:43:37 +0800

    fixed problem with arrays on root level for conditions

--- mapping-engine/mapping-core/src/test/java/org/eclipse/vorto/service/mapping/spec/SpecWithConditionFunction.java
+++ mapping-engine/mapping-core/src/test/java/org/eclipse/vorto/service/mapping/spec/SpecWithConditionFunction.java
@@ -13,27 +13,34 @@
 package org.eclipse.vorto.service.mapping.spec;
 
 import java.util.Arrays;
+
 import org.eclipse.vorto.model.FunctionblockModel;
 import org.eclipse.vorto.model.ModelId;
 import org.eclipse.vorto.model.ModelProperty;
 import org.eclipse.vorto.model.PrimitiveType;
 import org.eclipse.vorto.model.Stereotype;
 
-public class SpecWithArrayPayload extends AbstractTestSpec {
+public class SpecWithConditionFunction extends AbstractTestSpec {
 
   @Override
   protected void createFBSpec() {
     FunctionblockModel buttonModel = new FunctionblockModel(
         ModelId.fromPrettyFormat("demo.fb:PushButton:1.0.0"));
+    
+    buttonModel.setTargetPlatformKey("iotbutton");
+    buttonModel.addStereotype(Stereotype.createCondition("not empty(array[0])"));
+    
     ModelProperty digitalInputStateProperty = new ModelProperty();
     digitalInputStateProperty.setMandatory(true);
     digitalInputStateProperty.setName("sensor_value");
-    digitalInputStateProperty.setType(PrimitiveType.STRING);
+    digitalInputStateProperty.setType(PrimitiveType.FLOAT);
 
     digitalInputStateProperty.setTargetPlatformKey("iotbutton");
-    digitalInputStateProperty.addStereotype(Stereotype.createWithXpath("array[1]/clickType"));
+    digitalInputStateProperty
+        .addStereotype(Stereotype.createWithXpath("array[1]"));
 
-    buttonModel.setStatusProperties(Arrays.asList(new ModelProperty[] {digitalInputStateProperty}));
+    buttonModel.setStatusProperties(
+        Arrays.asList(new ModelProperty[] {digitalInputStateProperty}));
 
     addFunctionblockProperty("button", buttonModel);
   }
