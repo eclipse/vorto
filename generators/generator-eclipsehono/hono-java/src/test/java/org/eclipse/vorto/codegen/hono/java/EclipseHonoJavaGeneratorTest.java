e32a96b118b939b2d46ed0cc1997a1bbe2accc7e
MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>
2019-08-19 12:59:12 +0800

    JunitTests
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>

---
+++ generators/generator-eclipsehono/hono-java/src/test/java/org/eclipse/vorto/codegen/hono/java/EclipseHonoJavaGeneratorTest.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.vorto.codegen.hono.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.vorto.utilities.reader.IModelWorkspace;
+import org.eclipse.vorto.utilities.reader.ModelWorkspaceReader;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.eclipse.vorto.core.api.model.ModelConversionUtils;
+import org.eclipse.vorto.core.api.model.datatype.Entity;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+import org.eclipse.vorto.core.api.model.mapping.MappingModel;
+import org.eclipse.vorto.model.ModelType;
+import org.eclipse.vorto.plugin.generator.*;
+import org.eclipse.vorto.plugin.generator.utils.DatatypeGeneratorTask;
+import org.eclipse.vorto.plugin.generator.utils.Generated;
+import org.eclipse.vorto.plugin.generator.utils.GenerationResultBuilder;
+import org.eclipse.vorto.plugin.generator.utils.GenerationResultZip;
+import org.eclipse.vorto.plugin.generator.utils.IFileTemplate;
+import org.eclipse.vorto.plugin.generator.utils.IGeneratedWriter;
+
+public class EclipseHonoJavaGeneratorTest {
+
+	@Mock
+	protected IFileTemplate<Entity> entityTemplate = Mockito.mock(IFileTemplate.class);
+
+	@Mock
+	protected IFileTemplate<org.eclipse.vorto.core.api.model.datatype.Enum> enumTemplate = Mockito
+			.mock(IFileTemplate.class);
+
+	@Before
+	public void beforeEach() throws Exception {
+	}
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@BeforeClass
+	public static void initParser() {
+		ModelWorkspaceReader.init();
+	}
+
+	/** Test case for checking whether the returned file is a Zip file */
+	@Test
+	public void checkResultZipFileHonoJava() throws Exception {
+		List<MappingModel> mappingModels = new ArrayList<>();
+		Map<String, String> configProperties = new HashMap<>();
+		IModelWorkspace workspace = IModelWorkspace.newReader()
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/SuperInfomodel.infomodel"),
+						ModelType.InformationModel)
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/SuperSuperFb.fbmodel"),
+						ModelType.Functionblock)
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/ColorLight.type"), ModelType.Datatype)
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/Light.type"), ModelType.Datatype)
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/Brightness.type"), ModelType.Datatype)
+				.addFile(getClass().getClassLoader().getResourceAsStream("dsls/Coffee_Enum.type"), ModelType.Datatype)
+				.read();
+		InformationModel model = (InformationModel) workspace.get().stream().filter(p -> p instanceof InformationModel)
+				.findAny().get();
+		InvocationContext context = new InvocationContext(mappingModels, configProperties);
+
+		EclipseHonoJavaGenerator eclipseHonoJavaGenerator = new EclipseHonoJavaGenerator();
+		IGenerationResult generationResult = null;
+		generationResult = eclipseHonoJavaGenerator.generate(model, context);
+		assertTrue(generationResult.getFileName().endsWith("zip"));
+	}
+
+}
