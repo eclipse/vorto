a09a44b80709f118c21be44e30718c95f5a4e32e
MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>
2019-08-21 14:32:38 +0800

    JunitTests
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>

--- plugin-sdk/plugin-api/src/test/java/org/eclipse/vorto/plugin/AbstractGeneratorTest.java
+++ plugin-sdk/plugin-api/src/test/java/org/eclipse/vorto/plugin/AbstractGeneratorTest.java
@@ -2,7 +2,12 @@ package org.eclipse.vorto.plugin;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
 import org.eclipse.vorto.model.ModelType;
@@ -23,16 +28,38 @@ public class AbstractGeneratorTest {
 		ModelWorkspaceReader.init();
 	}
 
-	public InformationModel modelProvider(String infomodel, String functionBlock) {
-		IModelWorkspace workspace = IModelWorkspace.newReader()
+	public InformationModel modelProvider(String infomodel, String functionBlock,String dataType) {
+		ModelWorkspaceReader modelWorkspaceReader = IModelWorkspace.newReader()
 				.addFile(getClass().getClassLoader().getResourceAsStream(folderPath + infomodel),
 						ModelType.InformationModel)
 				.addFile(getClass().getClassLoader().getResourceAsStream(folderPath + functionBlock),
-						ModelType.Functionblock)
-				.read();
+						ModelType.Functionblock);
+		if(!dataType.isEmpty()) {
+			modelWorkspaceReader.addFile(getClass().getClassLoader().getResourceAsStream(folderPath + dataType),
+						ModelType.Datatype);
+		}
+		IModelWorkspace workspace = modelWorkspaceReader.read();
 		InformationModel model = (InformationModel) workspace.get().stream().filter(p -> p instanceof InformationModel)
 				.findAny().get();
 		return model;
+	}
+
+	public List<ZipEntry> extractZipEntries(IGenerationResult generationResult) throws IOException {
+		List<ZipEntry> entries = new ArrayList<>();
+		ZipInputStream zi = null;
+		try {
+			zi = new ZipInputStream(new ByteArrayInputStream(generationResult.getContent()));
+
+			ZipEntry zipEntry = null;
+			while ((zipEntry = zi.getNextEntry()) != null) {
+				entries.add(zipEntry);
+			}
+		} finally {
+			if (zi != null) {
+				zi.close();
+			}
+		}
+		return entries;
 	}
 
 	public void checkResultZipFile(ICodeGenerator iCodeGenerator, InformationModel model)
@@ -46,4 +73,5 @@ public class AbstractGeneratorTest {
 				InvocationContext.simpleInvocationContext());
 		return generationResult;
 	}
+
 }
