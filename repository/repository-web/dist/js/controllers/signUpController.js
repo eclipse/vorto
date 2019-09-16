22b3c49263c35d1a68fd1efa2594486085311687
Erle Czar Mantos <erleczars.mantos@bosch-si.com>
2019-09-16 19:45:01 +0800

    Fixes #1936 - User can bypass the T&C during first login (#1951)
    Signed-off-by: Erle Czar Mantos <erleczar.mantos@bosch-si.com>

--- repository/repository-web/dist/js/controllers/signUpController.js
+++ repository/repository-web/dist/js/controllers/signUpController.js
@@ -1,26 +1,55 @@
 define(["../init/appController"],function(repositoryControllers) {
 
-repositoryControllers.controller('SignUpController', [ '$location', '$rootScope', '$scope', '$http', '$routeParams', 
-	function ($location, $rootScope, $scope, $http, $routeParams) {
+repositoryControllers.controller('SignUpController', [ '$location', '$rootScope', '$scope', '$http', '$routeParams', "$uibModal", 
+	function ($location, $rootScope, $scope, $http, $routeParams, $uibModal) {
 
 	$scope.acceptTerms = false;
+	$scope.acceptPrivacyPolicy = false;
+	$scope.isAcceptingTermsAndCondition = false;
 	
-	var isAcceptingTermsAndCondition = false;
-	
-    $scope.acceptTermsAndConditions = function() {
-    	isAcceptingTermsAndCondition = true;
-    	
-        $http.post('./rest/accounts', {
-            headers: {'Content-Type': "application/json"}
-        })
-        .success( function(data, status, headers, config) {
-            isAcceptingTermsAndCondition = false;
-            $rootScope.init();
-            $location.path('/');
-        }).error(function(data, status, headers, config) {
-            isAcceptingTermsAndCondition = false;
+	$scope.runTermsAndConditions = function() {
+    	var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: "signup.html",
+            size: "lg",
+            controller: function($scope) {
+            	$scope.isAcceptingTermsAndCondition = false;
+    			
+    		    $scope.acceptTermsAndConditions = function() {
+    		    	$scope.isAcceptingTermsAndCondition = true;
+    		    	
+    		        $http.post('./rest/accounts', {
+    		            headers: {'Content-Type': "application/json"}
+    		        })
+    		        .success( function(data, status, headers, config) {
+    		        	$scope.isAcceptingTermsAndCondition = false;
+    		            $rootScope.init();
+    		            $location.path('/');
+    		            modalInstance.dismiss();
+    		        }).error(function(data, status, headers, config) {
+    		        	$scope.isAcceptingTermsAndCondition = false;
+    		        });
+    		    };
+    		    
+    		    $scope.showPrivacyPolicy =  function() {
+    				var privacyPolicyModalInstance = $uibModal.open({
+    		            animation: true,
+    		            templateUrl: "webjars/repository-web/dist/partials/privacypolicy-dialog.html",
+    		            size: "lg",
+    		            controller: function($scope) {
+    		            	$scope.cancel = function () {
+    		            		privacyPolicyModalInstance.dismiss();
+    						};
+    		            },
+    		            backdrop: 'static'
+    		        });
+    			};
+            },
+            backdrop: 'static'
         });
-}
+    };
+    
+    $scope.runTermsAndConditions();
 }]);
 
 });
