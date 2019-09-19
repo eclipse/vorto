172ab9364032f0dd3ffc734170c2e4488f7ca2d3
MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>
2019-09-19 16:33:17 +0800

    Fixes #1933_Invalid_warning_message_for_Test_Mapping_integer_configuration_property
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>

--- model-runtime/src/main/java/org/eclipse/vorto/model/runtime/FunctionblockValue.java
+++ model-runtime/src/main/java/org/eclipse/vorto/model/runtime/FunctionblockValue.java
@@ -24,163 +24,173 @@ import org.eclipse.vorto.model.PrimitiveType;
 
 public class FunctionblockValue implements IValidatable {
 
-  private FunctionblockModel meta;
+	private FunctionblockModel meta;
 
-  private List<PropertyValue> status = new ArrayList<PropertyValue>();
-  private List<PropertyValue> configuration = new ArrayList<PropertyValue>();
-  private List<FBEventValue> events = new ArrayList<FBEventValue>();
+	private List<PropertyValue> status = new ArrayList<PropertyValue>();
+	private List<PropertyValue> configuration = new ArrayList<PropertyValue>();
+	private List<FBEventValue> events = new ArrayList<FBEventValue>();
 
-  public FunctionblockValue(FunctionblockModel meta) {
-    this.meta = meta;
-  }
+	public FunctionblockValue(FunctionblockModel meta) {
+		this.meta = meta;
+	}
 
-  public FunctionblockModel getMeta() {
-    return this.meta;
-  }
+	public FunctionblockModel getMeta() {
+		return this.meta;
+	}
 
-  public List<PropertyValue> getStatus() {
-    return Collections.unmodifiableList(this.status);
-  }
+	public List<PropertyValue> getStatus() {
+		return Collections.unmodifiableList(this.status);
+	}
 
-  public Optional<PropertyValue> getStatusProperty(String propertyName) {
-    return this.status.stream().filter(p -> p.getMeta().getName().equals(propertyName)).findAny();
-  }
+	public Optional<PropertyValue> getStatusProperty(String propertyName) {
+		return this.status.stream().filter(p -> p.getMeta().getName().equals(propertyName)).findAny();
+	}
 
-  public List<PropertyValue> getConfiguration() {
-    return Collections.unmodifiableList(this.configuration);
-  }
+	public List<PropertyValue> getConfiguration() {
+		return Collections.unmodifiableList(this.configuration);
+	}
 
-  public Optional<PropertyValue> getConfigurationProperty(String propertyName) {
-    return this.configuration.stream().filter(p -> p.getMeta().getName().equals(propertyName))
-        .findAny();
-  }
+	public Optional<PropertyValue> getConfigurationProperty(String propertyName) {
+		return this.configuration.stream().filter(p -> p.getMeta().getName().equals(propertyName)).findAny();
+	}
 
-  public FunctionblockValue withStatusProperty(String name, Object value) {
-    Optional<ModelProperty> mp = meta.getStatusProperty(name);
-    if (!mp.isPresent()) {
-      throw new IllegalArgumentException(
-          "Status property with given name is not defined in Function Block");
-    }
+	public FunctionblockValue withStatusProperty(String name, Object value) {
+		Optional<ModelProperty> mp = meta.getStatusProperty(name);
+		if (!mp.isPresent()) {
+			throw new IllegalArgumentException("Status property with given name is not defined in Function Block");
+		}
 
-    Optional<PropertyValue> pv = this.getStatusProperty(name);
-    if (pv.isPresent()) {
-      pv.get().setValue(value);
-    } else {
-      
-      this.status.add(PropertyValueFactory.create(mp.get(),value));
-    }
+		Optional<PropertyValue> pv = this.getStatusProperty(name);
+		if (pv.isPresent()) {
+			pv.get().setValue(value);
+		} else {
 
-    return this;
-  }
+			this.status.add(PropertyValueFactory.create(mp.get(), value));
+		}
 
-  public FunctionblockValue withConfigurationProperty(String name, Object value) {
-    Optional<ModelProperty> mp = meta.getConfigurationProperty(name);
-    if (!mp.isPresent()) {
-      throw new IllegalArgumentException(
-          "Configuration property with given name is not defined in Function Block");
-    }
+		return this;
+	}
 
-    Optional<PropertyValue> pv = this.getConfigurationProperty(name);
-    if (pv.isPresent()) {
-      pv.get().setValue(value);
-    } else {
-      this.configuration.add(PropertyValueFactory.create(mp.get(), value));
-    }
+	public FunctionblockValue withConfigurationProperty(String name, Object value) {
+		Optional<ModelProperty> mp = meta.getConfigurationProperty(name);
+		if (!mp.isPresent()) {
+			throw new IllegalArgumentException(
+					"Configuration property with given name is not defined in Function Block");
+		}
 
-    return this;
-  }
+		Optional<PropertyValue> pv = this.getConfigurationProperty(name);
+		if (pv.isPresent()) {
+			pv.get().setValue(value);
+		} else {
+			this.configuration.add(PropertyValueFactory.create(mp.get(), value));
+		}
 
-  public FunctionblockValue withEvent(FBEventValue event) {
-    this.events.add(event);
-    return this;
-  }
+		return this;
+	}
 
-  @Override
-  public String toString() {
-    return "FunctionblockData [status=" + status + ", configuration=" + configuration + "]";
-  }
+	public FunctionblockValue withEvent(FBEventValue event) {
+		this.events.add(event);
+		return this;
+	}
 
-  @Override
-  public ValidationReport validate() {
-    ValidationReport report = new ValidationReport();
+	@Override
+	public String toString() {
+		return "FunctionblockData [status=" + status + ", configuration=" + configuration + "]";
+	}
 
-    for (ModelProperty statusProperty : meta.getStatusProperties()) {
-      checkProperty(getStatus(), statusProperty, meta.getId().getName().toLowerCase() + "",
-          report);
-    }
+	@Override
+	public ValidationReport validate() {
+		ValidationReport report = new ValidationReport();
 
-    for (ModelProperty configProperty : meta.getConfigurationProperties()) {
-      checkProperty(getConfiguration(), configProperty,
-          meta.getId().getName().toLowerCase() + "", report);
-    }
-    return report;
-  }
+		for (ModelProperty statusProperty : meta.getStatusProperties()) {
+			checkProperty(getStatus(), statusProperty, meta.getId().getName().toLowerCase() + "", report);
+		}
 
-  private void checkProperty(List<PropertyValue> properties, ModelProperty property, String path,
-      ValidationReport report) {
-    Optional<PropertyValue> mpd =
-        properties.stream().filter(p -> p.getMeta().equals(property)).findAny();
-    if (property.isMandatory() && !mpd.isPresent()) {
-      report.addItem(property,
-          "Mandatory field " + path + "/" + property.getName() + " is missing");
-    } else {
-      if (mpd.isPresent() && property.getType() instanceof PrimitiveType) {
-        checkPrimitiveTypeValue(path, mpd.get().getValue(), property, report);
-      }
-    }
-  }
+		for (ModelProperty configProperty : meta.getConfigurationProperties()) {
+			checkProperty(getConfiguration(), configProperty, meta.getId().getName().toLowerCase() + "", report);
+		}
+		return report;
+	}
 
-  private static void checkPrimitiveTypeValue(String path, Object propertyValue,
-      ModelProperty property, ValidationReport report) {
-    PrimitiveType type = (PrimitiveType) property.getType();
-    if (type == PrimitiveType.STRING && !(propertyValue instanceof String)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'String'");
-    } else if (type == PrimitiveType.BOOLEAN && !(propertyValue instanceof Boolean)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'Boolean'");
-    } else if (type == PrimitiveType.DOUBLE && !(propertyValue instanceof Double)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'Double'");
-    } else if (type == PrimitiveType.FLOAT && !isFloat(propertyValue)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'Float'");
-    } else if (type == PrimitiveType.INT && !isInteger(propertyValue)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'Integer'");
-    } else if (type == PrimitiveType.LONG && !isLong(propertyValue)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be of type 'Long'");
-    } else if (type == PrimitiveType.BASE64_BINARY && !(propertyValue instanceof String)) {
-      report.addItem(property,
-          "Field " + path + "/" + property.getName() + " must be a Base64-encoded 'String'");
-    }
-  }
+	private void checkProperty(List<PropertyValue> properties, ModelProperty property, String path,
+			ValidationReport report) {
+		Optional<PropertyValue> mpd = properties.stream().filter(p -> p.getMeta().equals(property)).findAny();
+		if (property.isMandatory() && !mpd.isPresent()) {
+			report.addItem(property, "Mandatory field " + path + "/" + property.getName() + " is missing");
+		} else {
+			if (mpd.isPresent() && property.getType() instanceof PrimitiveType) {
+				checkPrimitiveTypeValue(path, mpd.get().getValue(), property, report);
+			}
+		}
+	}
 
-  private static boolean isInteger(Object value) {
-    return value instanceof Integer;
-  }
+	private static void checkPrimitiveTypeValue(String path, Object propertyValue, ModelProperty property,
+			ValidationReport report) {
+		PrimitiveType type = (PrimitiveType) property.getType();
+		if (type == PrimitiveType.STRING && !(propertyValue instanceof String)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'String'");
+		} else if (type == PrimitiveType.BOOLEAN && !(propertyValue instanceof Boolean)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Boolean'");
+		} else if (type == PrimitiveType.DOUBLE && !(propertyValue instanceof Double)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Double'");
+		} else if (type == PrimitiveType.FLOAT && !isFloat(propertyValue)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Float'");
+		}
+		/*
+		 * The below check for integer being a double is added due to a bug in JXpath
+		 * which uses XPath 1.0, when any number (Integer/Double) is passed in XPath version 1.0, it is
+		 * always passed as a Double
+		 * (https://stackoverflow.com/questions/4721488/xpath-query-value-comparison-
+		 * problem).
+		 * (https://commons.apache.org/proper/commons-jxpath/).
+		 * To bypass the above, whenever a number is given in Integer type, it will be checked for double,
+		 * also it will be checked if its a whole number (whether it is a whole number like 1.0/2.0),
+		 * if not then validation fails for values such as 1.1, 2.3 etc.
+		 * 
+		 */
+		else if (type == PrimitiveType.INT && !(propertyValue instanceof Double)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Integer'");
+		} else if (type == PrimitiveType.INT && !(isDoubleInteger(propertyValue))) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Integer'");
+		} else if (type == PrimitiveType.LONG && !isLong(propertyValue)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be of type 'Long'");
+		} else if (type == PrimitiveType.BASE64_BINARY && !(propertyValue instanceof String)) {
+			report.addItem(property, "Field " + path + "/" + property.getName() + " must be a Base64-encoded 'String'");
+		}
+	}
 
-  private static boolean isFloat(Object value) {
-    return value instanceof Float || value instanceof Double;
-  }
+	private static boolean isInteger(Object value) {
+		return value instanceof Integer;
+	}
 
-  private static boolean isLong(Object value) {
-    return value instanceof Integer || value instanceof Long;
-  }
+	private static boolean isDoubleInteger(Object value) {
+		double i = (Double) value;
+		if (i % 1 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
+	private static boolean isFloat(Object value) {
+		return value instanceof Float || value instanceof Double;
+	}
 
-  public Map<String, Object> serialize() {
-    Map<String, Object> result = new HashMap<String, Object>();
+	private static boolean isLong(Object value) {
+		return value instanceof Integer || value instanceof Long;
+	}
 
-    for (PropertyValue statusProperty : status) {
-      result.put(statusProperty.getMeta().getName(), statusProperty.serialize());
-    }
+	public Map<String, Object> serialize() {
+		Map<String, Object> result = new HashMap<String, Object>();
 
-    for (PropertyValue configProperty : this.configuration) {
-      result.put(configProperty.getMeta().getName(), configProperty.serialize());
-    }
+		for (PropertyValue statusProperty : status) {
+			result.put(statusProperty.getMeta().getName(), statusProperty.serialize());
+		}
 
-    return result;
-  }
+		for (PropertyValue configProperty : this.configuration) {
+			result.put(configProperty.getMeta().getName(), configProperty.serialize());
+		}
+
+		return result;
+	}
 }
--- model-runtime/src/test/java/org/eclipse/vorto/model/runtime/ValidationTest.java
+++ model-runtime/src/test/java/org/eclipse/vorto/model/runtime/ValidationTest.java
@@ -32,14 +32,25 @@ public class ValidationTest {
     assertFalse(data.withStatusProperty("prop", 2).validate().isValid());
   }
 
+  /*
+	 * The below check for integer being a double is added due to a bug in JXpath
+	 * which uses XPath 1.0, when any number (Integer/Double) is passed in XPath version 1.0, it is
+	 * always passed as a Double
+	 * (https://stackoverflow.com/questions/4721488/xpath-query-value-comparison-
+	 * problem).
+	 * (https://commons.apache.org/proper/commons-jxpath/).
+	 * To bypass the above, whenever a number is given in Integer type, it will be checked for double,
+	 * also it will be checked if its a whole number (whether it is a whole number like 1.0/2.0),
+	 * if not then validation fails for values such as 1.1, 2.3 etc.
+	 * 
+	 */
   @Test
   public void testPrimitiveTypeValidationInteger() {
     FunctionblockValue data = new FunctionblockValue(createModel(PrimitiveType.INT));
-    assertTrue(data.withStatusProperty("prop", 2).validate().isValid());
+    assertFalse(data.withStatusProperty("prop", 2).validate().isValid());
     assertFalse(data.withStatusProperty("prop", "2").validate().isValid());
     assertFalse(data.withStatusProperty("prop", 2.2).validate().isValid());
-
-    assertFalse(data.withStatusProperty("prop", 2.0).validate().isValid());
+    assertTrue(data.withStatusProperty("prop", 2.0).validate().isValid());
   }
 
   @Test
