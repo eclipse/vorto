315637cc1bdcea4f83456c5f6f7be143ffddc518
aedelmann <alexander.edelmann@bosch-si.com>
2019-10-08 09:41:31 +0800

    removed validation upon storing the files to modeshape

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/importer/AbstractModelImporter.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/importer/AbstractModelImporter.java
@@ -20,7 +20,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import org.apache.log4j.Logger;
