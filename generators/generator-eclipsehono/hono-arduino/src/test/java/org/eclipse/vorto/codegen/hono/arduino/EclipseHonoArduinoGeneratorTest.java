9bc1bed00f5d7e71d4e77023eca2655c813dce6a
MayilKannanKumarBosch <50613608+MayilKannanKumarBosch@users.noreply.github.com>
2019-10-10 18:13:09 +0800

    JUnit9 (#2003)
    * JUnit9
    
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>
    
    * JUnit9
    
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>

--- generators/generator-eclipsehono/hono-arduino/src/test/java/org/eclipse/vorto/codegen/hono/arduino/EclipseHonoArduinoGeneratorTest.java
+++ generators/generator-eclipsehono/hono-arduino/src/test/java/org/eclipse/vorto/codegen/hono/arduino/EclipseHonoArduinoGeneratorTest.java
@@ -1,12 +1,11 @@
 /**
  * Copyright (c) 2018 Contributors to the Eclipse Foundation
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
+ * See the NOTICE file(s) distributed with this work for additional information regarding copyright
+ * ownership.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
  */
@@ -15,6 +14,7 @@ package org.eclipse.vorto.codegen.hono.arduino;
 import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.vorto.plugin.AbstractGeneratorTest;
@@ -27,148 +27,157 @@ import org.junit.Test;
 
 public class EclipseHonoArduinoGeneratorTest extends AbstractGeneratorTest {
 
-	ICodeGenerator eclipseArduinoGenerator = new ArduinoCodeGenerator();
+  ICodeGenerator eclipseArduinoGenerator = new ArduinoCodeGenerator();
 
-	/*
-	 * -----Below test cases are specific to API related files-------
-	 */
+  /*
+   * -----Below test cases are specific to API related files-------
+   */
 
-	/*
-	 * Test case for checking whether the functionblock related file has the status
-	 * properties both primitive and object with the setters and getters
-	 * 
-	 */
-	@Test
-	public void checkAPIStatusPropertiesInFunctionBlock() throws Exception {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the functionblock related file has the status properties both
+   * primitive and object with the setters and getters
+   * 
+   */
+  @Test
+  public void checkAPIStatusPropertiesInFunctionBlock() throws Exception {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "StatusPropertiesFunctionBlock.cpp", ".cpp");
+    Generated generatedfile =
+        zipFileReader(generationResult, "StatusPropertiesFunctionBlock.cpp", ".cpp");
 
-		File defaultFile = new File(getClass().getClassLoader()
-				.getResource("defaultFileFormat/StatusPropertiesFunctionBlock.cpp").getFile());
+    File defaultFile = new File(getClass().getClassLoader()
+        .getResource("defaultFileFormat/StatusPropertiesFunctionBlock.cpp").toURI());
 
-		assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-	}
+  }
 
-	/*
-	 * Test case for checking whether the functionblock related file has the config
-	 * properties both primitive and object with the setters and getters
-	 * 
-	 */
-	@Test
-	public void checkAPIConfigPropertiesInFunctionBlock() throws Exception {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the functionblock related file has the config properties both
+   * primitive and object with the setters and getters
+   * 
+   */
+  @Test
+  public void checkAPIConfigPropertiesInFunctionBlock() throws Exception {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "ConfigPropertiesFunctionBlock.cpp", ".cpp");
+    Generated generatedfile =
+        zipFileReader(generationResult, "ConfigPropertiesFunctionBlock.cpp", ".cpp");
 
-		File defaultFile = new File(getClass().getClassLoader()
-				.getResource("defaultFileFormat/ConfigPropertiesFunctionBlock.cpp").getFile());
+    File defaultFile = new File(getClass().getClassLoader()
+        .getResource("defaultFileFormat/ConfigPropertiesFunctionBlock.cpp").toURI());
 
-		assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-	}
+  }
 
-	/*
-	 * Test case for checking whether the datatype related file has the entity and
-	 * enum values specified in the input datatype file.
-	 * 
-	 */
-	@Test
-	public void checkAPIGenerationOfEntityModel() throws Exception {
+  /*
+   * Test case for checking whether the datatype related file has the entity and enum values
+   * specified in the input datatype file.
+   * 
+   */
+  @Test
+  public void checkAPIGenerationOfEntityModel() throws Exception {
 
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "UnitEntity.cpp", ".cpp");
+    Generated generatedfile = zipFileReader(generationResult, "UnitEntity.cpp", ".cpp");
 
-		File defaultFile = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/UnitEntity.cpp").getFile());
+    File defaultFile = new File(
+        getClass().getClassLoader().getResource("defaultFileFormat/UnitEntity.cpp").toURI());
 
-		assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-	}
+  }
 
-	/*
-	 * Test case for checking whether the events specified in the functionblock is
-	 * not reflected in the generated files. This is because we currently do not
-	 * support events.
-	 * 
-	 */
-	@Test
-	public void testAPIFunctionBlockWithEvents() throws Exception {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the events specified in the functionblock is not reflected in
+   * the generated files. This is because we currently do not support events.
+   * 
+   */
+  @Test
+  public void testAPIFunctionBlockWithEvents() throws Exception {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "eventsAndOperationsFunctionBlock.cpp", ".cpp");
+    Generated generatedfile =
+        zipFileReader(generationResult, "eventsAndOperationsFunctionBlock.cpp", ".cpp");
 
-		assertEquals(false, new String(generatedfile.getContent(),"utf-8").contains("testEvent"));
+    assertEquals(false, new String(generatedfile.getContent(), "utf-8").contains("testEvent"));
 
-	}
+  }
 
-	/*
-	 * Test case for checking whether the operations specified in the functionblock
-	 * is not reflected in the generated files. This is because we currently do not
-	 * support events.
-	 * 
-	 */
-	@Test
-	public void testAPIFunctionBlockWithOperations() throws Exception {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the operations specified in the functionblock is not reflected
+   * in the generated files. This is because we currently do not support events.
+   * 
+   */
+  @Test
+  public void testAPIFunctionBlockWithOperations() throws Exception {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "eventsAndOperationsFunctionBlock.cpp", ".cpp");
+    Generated generatedfile =
+        zipFileReader(generationResult, "eventsAndOperationsFunctionBlock.cpp", ".cpp");
 
-		assertEquals(false, new String(generatedfile.getContent(),"utf-8").contains("testOperation"));
+    assertEquals(false, new String(generatedfile.getContent(), "utf-8").contains("testOperation"));
 
-	}
+  }
 
-	/*
-	 * -----Below test cases are specific to Connector related files-------
-	 */
+  /*
+   * -----Below test cases are specific to Connector related files-------
+   */
 
-	/*
-	 * Test case for checking whether the MQTT client generated contains the models
-	 * which are listed in the information model
-	 * 
-	 */
-	@Test
-	public void testConnectorGeneratedMQTTClient() throws GeneratorException, IOException {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the MQTT client generated contains the models which are listed
+   * in the information model
+   * 
+   */
+  @Test
+  public void testConnectorGeneratedMQTTClient()
+      throws GeneratorException, IOException, URISyntaxException {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedFile = zipFileReader(generationResult, "MySensorApp.ino", ".ino");
+    Generated generatedFile = zipFileReader(generationResult, "MySensorApp.ino", ".ino");
 
-		File defaultFile = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/MySensorApp.ino").getFile());
+    File defaultFile = new File(
+        getClass().getClassLoader().getResource("defaultFileFormat/MySensorApp.ino").toURI());
 
-		assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedFile.getContent(),"utf-8"));
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedFile.getContent(), "utf-8"));
 
-	}
+  }
 
-	/*
-	 * -----Below test cases are specific to Client App related files-------
-	 */
+  /*
+   * -----Below test cases are specific to Client App related files-------
+   */
 
-	/*
-	 * Test case for checking whether the client App generated contains the models
-	 * which are listed in the information model
-	 * 
-	 */
-	@Test
-	public void testClientAppGeneratedModels() throws GeneratorException, IOException {
-		IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the client App generated contains the models which are listed in
+   * the information model
+   * 
+   */
+  @Test
+  public void testClientAppGeneratedModels()
+      throws GeneratorException, IOException, URISyntaxException {
+    IGenerationResult generationResult = eclipseArduinoGenerator.generate(modelProvider(),
+        InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "MySensor.cpp", ".cpp");
+    Generated generatedfile = zipFileReader(generationResult, "MySensor.cpp", ".cpp");
 
-		File defaultFile = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/MySensor.cpp").getFile());
+    File defaultFile =
+        new File(getClass().getClassLoader().getResource("defaultFileFormat/MySensor.cpp").toURI());
 
-		assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),new String(generatedfile.getContent(),"utf-8"));
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-	}
+  }
 
 }
--- generators/generator-eclipsehono/hono-python/src/test/java/org/eclipse/vorto/codegen/hono/python/EclipseHonoPythonGeneratorTest.java
+++ generators/generator-eclipsehono/hono-python/src/test/java/org/eclipse/vorto/codegen/hono/python/EclipseHonoPythonGeneratorTest.java
@@ -1,12 +1,11 @@
 /**
  * Copyright (c) 2018 Contributors to the Eclipse Foundation
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
+ * See the NOTICE file(s) distributed with this work for additional information regarding copyright
+ * ownership.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0
  *
  * SPDX-License-Identifier: EPL-2.0
  */
@@ -15,6 +14,7 @@ package org.eclipse.vorto.codegen.hono.python;
 import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.vorto.plugin.AbstractGeneratorTest;
@@ -26,136 +26,145 @@ import org.junit.Test;
 
 public class EclipseHonoPythonGeneratorTest extends AbstractGeneratorTest {
 
-	PythonGenerator pythonGenerator = new PythonGenerator();
-	
-
-	/*
-	 * -----Below test cases are specific to API related files-------
-	 */
-
-	/*
-	 * Test case for checking whether the functionblock related python file has the
-	 * status properties both primitive and object with the setters and getters
-	 * 
-	 */
-	@Test
-	public void checkAPIStatusPropertiesInFunctionBlock() throws Exception {
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
-
-		Generated generatedfile = zipFileReader(generationResult, "StatusPropertiesFunctionBlock",".py");
-		File defaultFile = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/StatusPropertiesFunctionBlock.py").getFile());
-	    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
-
-	}
-
-	/*
-	 * Test case for checking whether the functionblock related python file has the
-	 * config properties both primitive and object with the setters and getters
-	 * 
-	 */
-	@Test
-	public void checkAPIConfigPropertiesInFunctionBlock() throws Exception {
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
-
-		Generated generatedfile = zipFileReader(generationResult, "ConfigPropertiesFunctionBlock",".py");
-		File defaultFile = new File(getClass().getClassLoader()
-				.getResource("defaultFileFormat/ConfigPropertiesFunctionBlock.py").getFile());
-	    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
-
-	}
+  PythonGenerator pythonGenerator = new PythonGenerator();
 
 
-	/*
-	 * Test case for checking whether the entity/enum
-	 * values specified in the input datatype file are available.
-	 * 
-	 */
-	@Test
-	public void checkAPIGenerationOfEntityModel() throws Exception {
+  /*
+   * -----Below test cases are specific to API related files-------
+   */
 
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the functionblock related python file has the status properties
+   * both primitive and object with the setters and getters
+   * 
+   */
+  @Test
+  public void checkAPIStatusPropertiesInFunctionBlock() throws Exception {
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
 
-		Generated generatedfile = zipFileReader(generationResult, "MySensorApp",".py");
-		assertEquals(true, new String(generatedfile.getContent(),"utf-8").contains("unitEnum"));
-	}
+    Generated generatedfile =
+        zipFileReader(generationResult, "StatusPropertiesFunctionBlock", ".py");
+    File defaultFile = new File(getClass().getClassLoader()
+        .getResource("defaultFileFormat/StatusPropertiesFunctionBlock.py").toURI());
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-	/*
-	 * Test case for checking whether the events specified in the functionblock is
-	 * not reflected in the generated files. This is because we currently do not
-	 * support events.
-	 * 
-	 */
-	@Test
-	public void testAPIFunctionBlockWithEvents() throws Exception {
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  }
 
-		Generated generatedfile = zipFileReader(generationResult, "eventsAndOperationsFunctionBlock",".py");
-		assertEquals(false, new String(generatedfile.getContent(),"utf-8").contains("testEvent"));
-	}
+  /*
+   * Test case for checking whether the functionblock related python file has the config properties
+   * both primitive and object with the setters and getters
+   * 
+   */
+  @Test
+  public void checkAPIConfigPropertiesInFunctionBlock() throws Exception {
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
 
-	/*
-	 * Test case for checking whether the operations specified in the functionblock
-	 * is not reflected in the generated files. This is because we currently do not
-	 * support events.
-	 * 
-	 */
-	@Test
-	public void testAPIFunctionBlockWithOperations() throws Exception {
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+    Generated generatedfile =
+        zipFileReader(generationResult, "ConfigPropertiesFunctionBlock", ".py");
+    File defaultFile = new File(getClass().getClassLoader()
+        .getResource("defaultFileFormat/ConfigPropertiesFunctionBlock.py").toURI());
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
 
-		Generated generatedfile = zipFileReader(generationResult, "eventsAndOperationsFunctionBlock",".py");
-		assertEquals(false, new String(generatedfile.getContent(),"utf-8").contains("testOperation"));
-	}
+  }
 
-	/*
-	 * -----Below test cases are specific to Connector related files-------
-	 */
 
-	/*
-	 * Test case for checking whether the MQTT client generated contains the models
-	 * which are listed in the information model
-	 * 
-	 */
-	@Test
-	public void testConnectorGeneratedMQTTClient() throws GeneratorException, IOException {
-		
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
+  /*
+   * Test case for checking whether the entity/enum values specified in the input datatype file are
+   * available.
+   * 
+   */
+  @Test
+  public void checkAPIGenerationOfEntityModel() throws Exception {
 
-		Generated generatedFileHonoMqttClient = zipFileReader(generationResult, "DittoSerializer",".py");
-		File defaultFileHonoMqttClient = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/DittoSerializer.py").getFile());
-		
-	    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFileHonoMqttClient)), new String(generatedFileHonoMqttClient.getContent(),"utf-8"));
-	
-	}
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
 
-	/*
-	 * -----Below test cases are specific to Client App related files-------
-	 */
+    Generated generatedfile = zipFileReader(generationResult, "MySensorApp", ".py");
+    assertEquals(true, new String(generatedfile.getContent(), "utf-8").contains("unitEnum"));
+  }
 
-	/*
-	 * Test case for checking whether the client App generated contains the models
-	 * which are listed in the information model
-	 * 
-	 */
-	@Test
-	public void testClientAppGeneratedModels() throws GeneratorException, IOException {
-		IGenerationResult generationResult = pythonGenerator.generate(modelProvider(),
-				InvocationContext.simpleInvocationContext());
-		Generated generatedfile = zipFileReader(generationResult, "MySensorApp",".py");
-		
-		File defaultFile = new File(
-				getClass().getClassLoader().getResource("defaultFileFormat/MainApp.py").getFile());
-	    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)), new String(generatedfile.getContent(),"utf-8"));
+  /*
+   * Test case for checking whether the events specified in the functionblock is not reflected in
+   * the generated files. This is because we currently do not support events.
+   * 
+   */
+  @Test
+  public void testAPIFunctionBlockWithEvents() throws Exception {
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
 
-	}
+    Generated generatedfile =
+        zipFileReader(generationResult, "eventsAndOperationsFunctionBlock", ".py");
+    assertEquals(false, new String(generatedfile.getContent(), "utf-8").contains("testEvent"));
+  }
+
+  /*
+   * Test case for checking whether the operations specified in the functionblock is not reflected
+   * in the generated files. This is because we currently do not support events.
+   * 
+   */
+  @Test
+  public void testAPIFunctionBlockWithOperations() throws Exception {
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
+
+    Generated generatedfile =
+        zipFileReader(generationResult, "eventsAndOperationsFunctionBlock", ".py");
+    assertEquals(false, new String(generatedfile.getContent(), "utf-8").contains("testOperation"));
+  }
+
+  /*
+   * -----Below test cases are specific to Connector related files-------
+   */
+
+  /*
+   * Test case for checking whether the MQTT client generated contains the models which are listed
+   * in the information model
+   * 
+   */
+  @Test
+  public void testConnectorGeneratedMQTTClient()
+      throws GeneratorException, IOException, URISyntaxException {
+
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
+
+    Generated generatedFileHonoMqttClient =
+        zipFileReader(generationResult, "DittoSerializer", ".py");
+    File defaultFileHonoMqttClient = new File(
+        getClass().getClassLoader().getResource("defaultFileFormat/DittoSerializer.py").toURI());
+
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFileHonoMqttClient)),
+        new String(generatedFileHonoMqttClient.getContent(), "utf-8"));
+
+  }
+
+  /*
+   * -----Below test cases are specific to Client App related files-------
+   */
+
+  /*
+   * Test case for checking whether the client App generated contains the models which are listed in
+   * the information model
+   * 
+   */
+  @Test
+  public void testClientAppGeneratedModels()
+      throws GeneratorException, IOException, URISyntaxException {
+    IGenerationResult generationResult =
+        pythonGenerator.generate(modelProvider(), InvocationContext.simpleInvocationContext());
+    Generated generatedfile = zipFileReader(generationResult, "MySensorApp", ".py");
+
+    File defaultFile =
+        new File(getClass().getClassLoader().getResource("defaultFileFormat/MainApp.py").toURI());
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
+
+  }
 
 
 
