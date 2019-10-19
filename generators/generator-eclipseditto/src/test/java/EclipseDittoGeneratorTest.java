ea01695f289bf147199e96a92942025c67f71e22
MayilKannanKumarBosch <50613608+MayilKannanKumarBosch@users.noreply.github.com>
2019-10-19 13:43:17 +0800

    JUnit13 (#2031)
    Signed-off-by: MayilKannanKumarBosch <MayilKannan.Kumar@bosch-si.com>

---
+++ generators/generator-eclipseditto/src/test/java/EclipseDittoGeneratorTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.assertEquals;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.vorto.codegen.ditto.EclipseDittoGenerator;
+import org.eclipse.vorto.core.api.model.BuilderUtils;
+import org.eclipse.vorto.core.api.model.datatype.Entity;
+import org.eclipse.vorto.core.api.model.datatype.Enum;
+import org.eclipse.vorto.core.api.model.datatype.PrimitiveType;
+import org.eclipse.vorto.core.api.model.functionblock.Event;
+import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel;
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+import org.eclipse.vorto.core.api.model.model.ModelId;
+import org.eclipse.vorto.model.ModelType;
+import org.eclipse.vorto.plugin.AbstractGeneratorTest;
+import org.eclipse.vorto.plugin.generator.ICodeGenerator;
+import org.eclipse.vorto.plugin.generator.IGenerationResult;
+import org.eclipse.vorto.plugin.generator.InvocationContext;
+import org.eclipse.vorto.plugin.generator.utils.Generated;
+import org.eclipse.vorto.utilities.reader.IModelWorkspace;
+import org.eclipse.vorto.utilities.reader.ModelWorkspaceReader;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class EclipseDittoGeneratorTest extends AbstractGeneratorTest {
+  public static final String folderPath = "dsls/";
+
+  @BeforeClass
+  public static void initParser() {
+    ModelWorkspaceReader.init();
+  }
+
+  public InformationModel modelProvider(String infomodel, String functionBlock) {
+    IModelWorkspace workspace = IModelWorkspace.newReader()
+        .addFile(getClass().getClassLoader().getResourceAsStream(folderPath + infomodel),
+            ModelType.InformationModel)
+        .addFile(getClass().getClassLoader().getResourceAsStream(folderPath + functionBlock),
+            ModelType.Functionblock)
+        .read();
+    InformationModel model = (InformationModel) workspace.get().stream()
+        .filter(p -> p instanceof InformationModel).findAny().get();
+    
+    return model;
+  }
+
+  ICodeGenerator eclipseDittoGenerator = new EclipseDittoGenerator();
+
+  /*
+   * Test case for checking whether the multiple keyword generates corresponding array type in json
+   * 
+   */
+  @Test
+  public void checkMultipleKeywordInFunctionBlock() throws Exception {
+    IGenerationResult generationResult = eclipseDittoGenerator.generate(
+        modelProvider("MultiplTypeIm.infomodel", "MultipleTypeFb.functionblock"),
+        InvocationContext.simpleInvocationContext());
+
+    Generated generatedfile = zipFileReader(generationResult, "properties-status.schema", ".json");
+
+    File defaultFile = new File(getClass().getClassLoader()
+        .getResource("defaultFileFormat/properties-status.schema.json").toURI());
+
+    assertEquals(IOUtils.toString(FileUtils.openInputStream(defaultFile)),
+        new String(generatedfile.getContent(), "utf-8"));
+
+  }
+}
