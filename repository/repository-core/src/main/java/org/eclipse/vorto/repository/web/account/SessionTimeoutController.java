643d16d43c90d445e66b678d3cbb96e2893a0292
kolotu <kevin.olotu@bosch-si.com>
2019-11-12 14:25:12 +0100

    Add session timeout warning. Fixes #1735
    Signed-off-by: kolotu <kevin.olotu@bosch-si.com>

---
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/account/SessionTimeoutController.java
@@ -0,0 +1,19 @@
+package org.eclipse.vorto.repository.web.account;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionTimeoutController {
+
+  @GetMapping(path = "rest/config/session/timeout")
+  public ResponseEntity<Integer> getSessionTimeout(HttpServletRequest request) {
+    return new ResponseEntity<>(request.getSession().getMaxInactiveInterval(), HttpStatus.OK);
+  }
+
+}
