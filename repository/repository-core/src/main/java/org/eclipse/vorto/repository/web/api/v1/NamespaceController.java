cfe1cbd5191626308c432b4ddb75e9405303cae2
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-01-27 16:56:39 +0100

    refactoring/2152 Use namespace-based APIs instead of Tenant-based APIs
    Intermediate commit - some UI broken.
    
    * Cleaned up imports in NamespaceController
    
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/api/v1/NamespaceController.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/api/v1/NamespaceController.java
@@ -18,7 +18,6 @@ import java.security.Principal;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
