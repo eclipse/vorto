d9bdf1ff6f7c6792aaa87ccefd0477b694812ecd
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-02-04 17:18:21 +0100

    refactoring/2152 Use namespace-based APIs instead of Tenant-based APIs
    Intermediate commit - some UI broken.
    
    * Cosmetics: changed {tenantId} path variable to {namespace} in ModelResolveController. This is pure cosmetics as the variable is never used within the controller, and the controller is not API v1 nor used by the UI.
    
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/core/ModelResolveController.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/core/ModelResolveController.java
@@ -32,7 +32,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping(value = "/rest/{tenantId}/models/resolvers")
+@RequestMapping(value = "/rest/{namespace}/models/resolvers")
 @Api(value = "/resolve", description = "Resolve information models by mapped platform attributes")
 public class ModelResolveController {
 
