0a016b42173ddbcaefcbfebc34caaa240378ca29
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-02-19 09:27:56 +0100

    Cosmetic change in copyright header to pass the maven plugin
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/api/v1/NamespaceController.java
+++ repository/repository-core/src/main/java/org/eclipse/vorto/repository/web/api/v1/NamespaceController.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2020 Contributors to the Eclipse Foundation
  *
- * See the NOTICE file(s) distributed with this work for additional information regarding copyright
- * ownership.
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0
