dfa9282b37a41f944346137e8c5a5c63dc204af2
kolotu <kevin.olotu@bosch.io>
2020-02-25 16:50:01 +0100

    Fix whitespaces.
    Signed-off-by: kolotu <kevin.olotu@bosch.io>

--- generators/generator-eclipseditto/src/main/java/org/eclipse/vorto/codegen/ditto/EclipseDittoGenerator.java
+++ generators/generator-eclipseditto/src/main/java/org/eclipse/vorto/codegen/ditto/EclipseDittoGenerator.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2020 Contributors to the Eclipse Foundation
- * <p>
+ *
  * See the NOTICE file(s) distributed with this work for additional information regarding copyright
  * ownership.
- * <p>
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.vorto.codegen.ditto;
