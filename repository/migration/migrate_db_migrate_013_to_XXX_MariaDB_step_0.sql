4b54be96fff98afa2e15d76b58de7c2f53cf9cfa
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-03-09 13:43:52 +0100

    * MariaDB syntax script step 0 (not finished): multiline comments and some "air"
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/migration/migrate_db_migrate_013_to_XXX_MariaDB_step_0.sql
+++ repository/migration/migrate_db_migrate_013_to_XXX_MariaDB_step_0.sql
@@ -1,5 +1,7 @@
-# This procedure modifies the namespace table by replacing the tenant foreign key with a foreign
-# key pointing directly to the user who owns the namespace
+/*
+ This procedure modifies the namespace table by replacing the tenant foreign key with a foreign
+ key pointing directly to the user who owns the namespace.
+ */
 drop procedure if exists refactor_namespace_ownership_reference;
 
 create procedure refactor_namespace_ownership_reference()
@@ -9,14 +11,19 @@ begin
     set user_in_namespace_count = (
         select count(*) from information_schema.COLUMNS where TABLE_NAME='namespace' and COLUMN_NAME='user'
     );
-    # This does two things:
-        # 1 Creates a user field in the namespace table with reference to the primary key in the user table
-        # 2 Updates the values of user in namespace to match the user id referenced in tenant_user,
-        # through a nested inner join
-        # Note: the owner_id of the tenant table is unfortunately missing data, so it is not usable
-        # (hence, the nexted inner join). Likely the historical reason is that the owner_id field
-        # has been abandoned back then, in favor of the intermediary table tenant_user, which we are
-        # ultimately going to remove again.
+    /*
+        This does two things:
+
+            1) Creates a user field in the namespace table with reference to the primary key in the user table
+            2) Updates the values of user in namespace to match the user id referenced in tenant_user,
+            through a nested inner join
+
+        Note:
+            The owner_id of the tenant table is unfortunately missing data, so it is not usable
+            hence, the nexted inner join). Likely the historical reason is that the owner_id field
+            has been abandoned back then, in favor of the intermediary table tenant_user, which we are
+            ultimately going to remove again.
+     */
     if (user_in_namespace_count = 0) then
         ALTER TABLE namespace ADD COLUMN user bigint;
         ALTER TABLE namespace ADD CONSTRAINT user FOREIGN KEY (user) references user (id);
