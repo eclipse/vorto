8fd98abf9d4fb10d99b761a194482ac8c5e6c522
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-03-25 17:09:21 +0100

    2354_cors
    * Restructured tests to use file-based resources
    * Header in test class
    
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/it/CORSRequestIntegrationTest.java
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/it/CORSRequestIntegrationTest.java
@@ -1,15 +1,41 @@
+/**
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.vorto.repository.server.it;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.file.Files;
+import java.util.List;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Tests for CORS requests with regards to any allowed Origin headers
  * (see application yml configuration).
  */
 public class CORSRequestIntegrationTest extends AbstractIntegrationTest {
+
+  private static List<String> testValidOrigins;
+  private static List<String> testInvalidOrigins;
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    testValidOrigins = Files.readAllLines(new ClassPathResource("origins/valid_origins.txt").getFile().toPath());
+    testInvalidOrigins = Files.readAllLines(new ClassPathResource("origins/invalid_origins.txt").getFile().toPath());
+  }
 
   @Override
   protected void setUpTest() throws Exception {
@@ -18,94 +44,52 @@ public class CORSRequestIntegrationTest extends AbstractIntegrationTest {
   }
 
   @Test
-  public void testRequestWithPassingOriginsForEclipseOrg() throws Exception {
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-        .header("Origin", "https://vorto.eclipse.org")
-        .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://vorto.eclipse.org")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "https://vorto.eclipse.org:8080")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://vorto.eclipse.org:8888")
-            .with(userCreator))
-        .andExpect(status().isOk());
+  public void testGETRequestsValid() throws Exception {
+    for (String origin: testValidOrigins) {
+      repositoryServer.perform(
+          get("/api/v1/models/" + testModel.prettyName)
+              .header("Origin", origin)
+              .with(userCreator))
+          .andExpect(status().isOk());
+    }
   }
 
   @Test
-  public void testRequestWithPassingOriginsForLocalhost() throws Exception {
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "https://localhost")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://localhost")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "https://localhost:8080")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://localhost:8888")
-            .with(userCreator))
-        .andExpect(status().isOk());
+  public void testGETRequestsInvalid() throws Exception {
+    for (String origin: testInvalidOrigins) {
+      repositoryServer.perform(
+          get("/api/v1/models/" + testModel.prettyName)
+              .header("Origin", origin)
+              .with(userCreator))
+          .andExpect(status().isForbidden());
+    }
   }
 
   @Test
-  public void testRequestWithPassingOriginsForBosch() throws Exception {
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "https://bosch-iot-suite.com")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://test.bosch-iot-suite.com")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "https://something.bosch-iot-cloud.com:8080")
-            .with(userCreator))
-        .andExpect(status().isOk());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://test.bosch-iot-suite.com:8888")
-            .with(userCreator))
-        .andExpect(status().isOk());
+  public void testDELETERequestsValid() throws Exception {
+    TestModel newModel = TestModel.TestModelBuilder.aTestModel().build();
+    for (String origin: testValidOrigins) {
+      newModel.createModel(repositoryServer, userCreator);
+      repositoryServer.perform(
+          delete("/rest/models/" + newModel.prettyName)
+              .header("Origin", origin)
+              .with(userCreator))
+          .andExpect(status().isOk());
+    }
   }
 
   @Test
-  public void testRequestWithFailingOrigins() throws Exception {
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://pwned.com")
-            .with(userCreator))
-        .andExpect(status().isForbidden());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "http://fakebosch-iot-suite.com:6666")
-            .with(userCreator))
-        .andExpect(status().isForbidden());
-    repositoryServer.perform(
-        get("/api/v1/models/" + testModel.prettyName)
-            .header("Origin", "mySpoofedOrigin")
-            .with(userCreator))
-        .andExpect(status().isForbidden());
+  public void testDELETERequestsInvalid() throws Exception {
+    TestModel newModel = TestModel.TestModelBuilder.aTestModel().build();
+    for (String origin: testInvalidOrigins) {
+      newModel.createModel(repositoryServer, userCreator);
+      repositoryServer.perform(
+          delete("/rest/models/" + newModel.prettyName)
+              .header("Origin", origin)
+              .with(userCreator))
+          .andExpect(status().isForbidden());
+    }
   }
+
 
 }
