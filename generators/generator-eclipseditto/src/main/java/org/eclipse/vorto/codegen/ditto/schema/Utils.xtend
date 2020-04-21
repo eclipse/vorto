94dfdae0d367dfed3291bc0ff41792b40d30215c
Tobias Gauss <tobias.gauss@bosch.io>
2020-04-21 19:16:29 +0200

    Duplicated properties in generators. Function block hierarchy in Ditto generator. Fixes #2376, Fixes #2374
    Signed-off-by: Tobias Gauss <tobias.gauss@bosch.io>

--- generators/generator-eclipseditto/src/main/java/org/eclipse/vorto/codegen/ditto/schema/Utils.xtend
+++ generators/generator-eclipseditto/src/main/java/org/eclipse/vorto/codegen/ditto/schema/Utils.xtend
@@ -14,8 +14,22 @@
  *******************************************************************************/
 package org.eclipse.vorto.codegen.ditto.schema
 
-import org.eclipse.vorto.codegen.ditto.schema.tasks.template.ConstraintTemplate
+import java.util.HashSet
+import java.util.List
+import java.util.Set
+import org.eclipse.emf.common.util.BasicEList
+import org.eclipse.emf.common.util.EList
+import org.eclipse.emf.ecore.util.EcoreUtil
+import org.eclipse.vorto.core.api.model.datatype.Entity
+import org.eclipse.vorto.core.api.model.datatype.Enum
+import org.eclipse.vorto.core.api.model.datatype.Property
 import org.eclipse.vorto.core.api.model.datatype.ConstraintRule
+import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel
+import org.eclipse.vorto.core.api.model.informationmodel.FunctionblockProperty
+import org.eclipse.vorto.core.api.model.informationmodel.InformationModel
+import org.eclipse.vorto.core.api.model.model.Model
+import org.eclipse.vorto.model.ModelId
+import org.eclipse.vorto.codegen.ditto.schema.tasks.template.ConstraintTemplate
 import org.eclipse.vorto.plugin.generator.InvocationContext
 
 class Utils {
@@ -30,4 +44,59 @@ class Utils {
 		«ENDIF»
 		'''
 	}
+
+		static def EList<FunctionblockModel> getReferencedFunctionBlocks(InformationModel infomodel) {
+    		var fbs = new BasicEList<FunctionblockModel>();
+    		for (FunctionblockProperty property : infomodel.properties) {
+    			if (fbs.filter[EcoreUtil.equals(it, property.type)].empty) {
+    				fbs.add(property.type);
+    			}
+    		}
+    		return fbs;
+    	}
+
+
+    	static def EList<FunctionblockModel> getFunctionBlockHierarchy(FunctionblockModel model) {
+        	var fbs = new BasicEList<FunctionblockModel>();
+        	    if(model.superType == null) {
+        	        fbs.add(model);
+                 } else {
+                    fbs.add(model);
+        	        fbs.addAll(getFunctionBlockHierarchy(model.superType));
+        	     }
+         	return fbs;
+        }
+
+    	static def Set<Entity> getReferencedEntities(InformationModel infomodel) {
+    		var entities = new HashSet<Entity>();
+    		for (FunctionblockProperty property : infomodel.properties) {
+    			for (Entity entity : org.eclipse.vorto.plugin.utils.Utils.getReferencedEntities(
+    				property.type.functionblock)) {
+    				if (entities.filter[createModelId(it).equals(createModelId(entity))].empty) {
+    					entities.add(entity);
+    				}
+    			}
+    		}
+    		return entities;
+    	}
+
+    	static def ModelId createModelId(Model model) {
+    		return new ModelId(model.name, model.namespace, model.version)
+    	}
+
+    	static def EList<Enum> getReferencedEnums(InformationModel infomodel) {
+    		var enums = new BasicEList<Enum>();
+    		for (FunctionblockProperty property : infomodel.properties) {
+    			for (Enum enumeration : org.eclipse.vorto.plugin.utils.Utils.getReferencedEnums(
+    				property.type.functionblock)) {
+    				if (enums.filter[EcoreUtil.equals(it, enumeration)].empty) {
+    					enums.add(enumeration);
+    				}
+    			}
+    		}
+    		return enums;
+    	}
+
+    	static def CharSequence calculateRequired(
+    		List<Property> properties) '''«IF !properties.filter[presence !== null && presence.mandatory].isEmpty»required: [«FOR property : properties.filter[presence !== null && presence.mandatory] SEPARATOR ','»«property.name»«ENDFOR»]«ENDIF»'''
 }
--- generators/generator-eclipseditto/src/test/java/EclipseDittoGeneratorTest.java
+++ generators/generator-eclipseditto/src/test/java/EclipseDittoGeneratorTest.java
@@ -10,45 +10,79 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.vorto.codegen.ditto.EclipseDittoGenerator;
+import org.eclipse.vorto.core.api.model.BuilderUtils;
+import org.eclipse.vorto.core.api.model.functionblock.FunctionblockModel;
 import org.eclipse.vorto.core.api.model.informationmodel.InformationModel;
+import org.eclipse.vorto.core.api.model.model.ModelId;
 import org.eclipse.vorto.model.ModelType;
 import org.eclipse.vorto.plugin.AbstractGeneratorTest;
+import org.eclipse.vorto.plugin.generator.GeneratorException;
 import org.eclipse.vorto.plugin.generator.ICodeGenerator;
 import org.eclipse.vorto.plugin.generator.IGenerationResult;
 import org.eclipse.vorto.plugin.generator.InvocationContext;
 import org.eclipse.vorto.plugin.generator.utils.Generated;
+import org.eclipse.vorto.plugin.utils.Utils;
 import org.eclipse.vorto.utilities.reader.IModelWorkspace;
 import org.eclipse.vorto.utilities.reader.ModelWorkspaceReader;
+import org.json.JSONException;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class EclipseDittoGeneratorTest extends AbstractGeneratorTest {
   public static final String folderPath = "dsls/";
+  private static final Map<String, ModelType> FILENAME_TO_TYPE_MAP = new HashMap<>();
+
+
+
+  static {
+    FILENAME_TO_TYPE_MAP.put("infomodel", ModelType.InformationModel);
+    FILENAME_TO_TYPE_MAP.put("functionblock", ModelType.Functionblock);
+    FILENAME_TO_TYPE_MAP.put("fbmodel", ModelType.Functionblock);
+    FILENAME_TO_TYPE_MAP.put("type", ModelType.Datatype);
+  }
+
 
   @BeforeClass
   public static void initParser() {
     ModelWorkspaceReader.init();
   }
 
-  public InformationModel modelProvider(String infomodel, String functionBlock) {
-    IModelWorkspace workspace = IModelWorkspace.newReader()
-        .addFile(getClass().getClassLoader().getResourceAsStream(folderPath + infomodel),
-            ModelType.InformationModel)
-        .addFile(getClass().getClassLoader().getResourceAsStream(folderPath + functionBlock),
-            ModelType.Functionblock)
-        .read();
+  public InformationModel modelProvider(String... filenames) {
+    ModelWorkspaceReader mwr = IModelWorkspace.newReader();
+    for(String filename : filenames) {
+      mwr.addFile(getClass().getClassLoader().getResourceAsStream(folderPath + filename),
+              FILENAME_TO_TYPE_MAP.get(FilenameUtils.getExtension(filename)));
+    }
+    IModelWorkspace workspace = mwr.read();
     InformationModel model = (InformationModel) workspace.get().stream()
-        .filter(p -> p instanceof InformationModel).findAny().get();
-
+            .filter(p -> p instanceof InformationModel).findAny().get();
     return model;
+  }
+
+
+  public InformationModel inheritanceFunctionBlockProvider(String rootFunctionBlockName, String... functionBlocks) {
+    ModelWorkspaceReader mwr = IModelWorkspace.newReader();
+
+    for (String parentFb : functionBlocks) {
+      mwr.addFile(getClass().getClassLoader().getResourceAsStream(folderPath + parentFb),
+              ModelType.Functionblock);
+    }
+    IModelWorkspace workspace = mwr.read();
+    FunctionblockModel fb = (FunctionblockModel) workspace.get().stream()
+            .filter(p -> p.getName().equals(rootFunctionBlockName)).findAny().get();
+    return Utils.toInformationModel(fb);
   }
 
   ICodeGenerator eclipseDittoGenerator = new EclipseDittoGenerator();
@@ -77,7 +111,6 @@ public class EclipseDittoGeneratorTest extends AbstractGeneratorTest {
 
   /*
    * Test case for checking whether the config properties are reflected in json
-   *
    */
   @Test
   public void checkAPIConfigPropertiesInFunctionBlock() throws Exception {
@@ -105,7 +138,160 @@ public class EclipseDittoGeneratorTest extends AbstractGeneratorTest {
     IGenerationResult generationResult = eclipseDittoGenerator.generate(
         modelProvider("MultiplTypeIm.infomodel", "MultipleTypeFb.functionblock"),
         InvocationContext.simpleInvocationContext(config));
-
     assertEquals("application/json", generationResult.getMediatype());
   }
+
+  /**
+   * Tests function block inheritance over two levels.
+   *
+   * (please be aware that the inherited properties are missing in the
+   * expected result because they are added by the flattening
+   * mechanism which takes place on repository side. Its not relevant for
+   * this test.)
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testFunctionBlockInheritance() throws Exception {
+    Map<String, String> params = new HashMap<>();
+    params.put("target", "thingJson");
+    IGenerationResult generationResult = eclipseDittoGenerator.generate(
+            inheritanceFunctionBlockProvider("SubTestFb", "ParentFbToBeExtended.functionblock", "SubTestFb.functionblock", "TestFb.functionblock"),
+            InvocationContext.simpleInvocationContext(params));
+    assertEquals("application/json", generationResult.getMediatype());
+
+    String expectedResult = "{\n" +
+            "  \"definition\": \"vortotest:SubTestFbIM:1.0.0\",\n" +
+            "  \"attributes\": {\n" +
+            " \t\"modelDisplayName\": \"SubTestFb\"\n" +
+            " \t },\n" +
+            " \t\"features\": {\n" +
+            " \t\"subtestfb\" : {\n" +
+            " \t\"definition\": [\n" +
+            " \t\t\"vortotest:SubTestFb:1.0.0\",\n" +
+            " \t\t\"vortotest:TestFb:1.0.0\",\n" +
+            " \t\t\"vortotest:ParentFbToBeExtended:1.0.0\"\n" +
+            " \t],\n" +
+            " \t\"properties\": {\n" +
+            " \t\t\"status\": {\n" +
+            " \t\t\t\"myFloatInSubTestFb\" : 0.0\n" +
+            " \t\t},\n" +
+            "\t\"configuration\": {\n" +
+            "\t\t\"temperatureSub\" : 0.0\n" +
+            "}\n" +
+            " \t}\n" +
+            " \t}\n" +
+            "}\n" +
+            "}";
+    JSONAssert.assertEquals(expectedResult, new String(generationResult.getContent()), true);
+  }
+
+  /**
+   * Tests function block inheritance with multiple function blocks.
+   * (please be aware that the inherited properties are missing in the
+   * expected result because they are added by the flattening
+   * mechanism which takes place on repository side. Its not relevant for
+   * this test.)
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testFunctionBlockInheritanceMultipleFunctionBlocks() throws Exception {
+    Map<String, String> params = new HashMap<>();
+    params.put("target", "thingJson");
+    IGenerationResult generationResult = eclipseDittoGenerator.generate(
+            modelProvider("Battery.fbmodel", "Location.fbmodel", "Percentage.type", "RaspberryPi.infomodel", "SensorValue.type","Temperature.fbmodel", "Voltage.fbmodel"),
+            InvocationContext.simpleInvocationContext(params));
+    assertEquals("application/json", generationResult.getMediatype());
+
+    String expectedResult = "{\n" +
+            "  \"definition\": \"org.eclipse.vorto.tutorials:RaspberryPi:1.0.0\",\n" +
+            "  \"attributes\": {\n" +
+            " \t\"modelDisplayName\": \"RaspberryPi\"\n" +
+            " \t },\n" +
+            " \t\"features\": {\n" +
+            " \t\"location\" : {\n" +
+            " \t\"definition\": [\n" +
+            " \t \t\t\"org.eclipse.vorto:Location:1.0.0\"\n" +
+            " \t],\n" +
+            " \t\"properties\": {\n" +
+            " \t\t\"status\": {\n" +
+            " \t\t\t\"latitude\" : 0.0,\n" +
+            " \t\t\t\"longitude\" : 0.0\n" +
+            " \t\t}\n" +
+            " \t}\n" +
+            " \t},\n" +
+            " \t\"battery\" : {\n" +
+            " \t\"definition\": [\n" +
+            " \t \t\t\"org.eclipse.vorto:Battery:1.0.0\",\n" +
+            " \t \t\t\"org.eclipse.vorto:Voltage:1.0.0\"\n" +
+            " \t],\n" +
+            " \t\"properties\": {\n" +
+            " \t\t\"status\": {\n" +
+            " \t\t\t\"remainingCapacity\" : {\n" +
+            " \t\t\t\t\"value\" : 0.0\n" +
+            " \t\t\t}\n" +
+            " \t\t},\n" +
+            "\t\"configuration\": {\n" +
+            "\t\t\"remainingCapacityAmpHour\" : 0.0\n" +
+            "}\n" +
+            " \t}\n" +
+            " \t},\n" +
+            " \t\"cpuTemperature\" : {\n" +
+            " \t\"definition\": [\n" +
+            " \t \t\t\"org.eclipse.vorto:Temperature:1.0.0\"\n" +
+            " \t],\n" +
+            " \t\"properties\": {\n" +
+            " \t\t\"status\": {\n" +
+            " \t\t\t\"value\" : {\n" +
+            " \t\t\t\t\"currentMeasured\" : 0.0,\n" +
+            " \t\t\t\t\"minMeasured\" : 0.0,\n" +
+            " \t\t\t\t\"maxMeasured\" : 0.0\n" +
+            " \t\t\t}\n" +
+            " \t\t}\n" +
+            " \t}\n" +
+            " \t}\n" +
+            "}\n" +
+            "}";
+
+    JSONAssert.assertEquals(expectedResult, new String(generationResult.getContent()), true);
+  }
+
+  @Test
+  public void testNonInheritanceFunctionBlock() throws GeneratorException, JSONException {
+
+    BuilderUtils.EnumBuilder enumBuilder = BuilderUtils.newEnum(new ModelId(org.eclipse.vorto.core.api.model.model.ModelType.Datatype, "Units", "org.eclipse.vorto.types", "1.0.0"));
+    enumBuilder.withLiterals("F", "C");
+
+    FunctionblockModel fbm = BuilderUtils.newFunctionblock(
+            new ModelId(org.eclipse.vorto.core.api.model.model.ModelType.Functionblock, "Temperature", "org.eclipse.vorto", "1.0.0")).withStatusProperty("unit", enumBuilder.build()).build();
+
+    BuilderUtils.InformationModelBuilder informationModelBuilder = BuilderUtils.newInformationModel(
+            new ModelId(org.eclipse.vorto.core.api.model.model.ModelType.InformationModel, "RPi", "org.eclipse.vorto", "1.0.0"));
+    informationModelBuilder.withFunctionBlock(fbm, "cpuTemperature", null, false);
+
+    IGenerationResult generationResult = eclipseDittoGenerator.generate(informationModelBuilder.build(), InvocationContext.simpleInvocationContext());
+
+    String expectedResult = "{\n" +
+            "  \"definition\": \"org.eclipse.vorto:RPi:1.0.0\",\n" +
+            "  \"attributes\": {\n" +
+            " \t\"modelDisplayName\": \"RPi\"\n" +
+            " \t },\n" +
+            " \t\"features\": {\n" +
+            " \t\"cpuTemperature\" : {\n" +
+            " \t\"definition\": [\n" +
+            " \t\t\"org.eclipse.vorto:Temperature:1.0.0\"\n" +
+            " \t],\n" +
+            " \t\"properties\": {\n" +
+            " \t\t\"status\": {\n" +
+            " \t\t\t\"unit\" : \"F\"\n" +
+            " \t\t}\n" +
+            " \t}\n" +
+            " \t}\n" +
+            "}\n" +
+            "}";
+
+    JSONAssert.assertEquals(expectedResult, new String(generationResult.getContent()), true);
+  }
+
 }
