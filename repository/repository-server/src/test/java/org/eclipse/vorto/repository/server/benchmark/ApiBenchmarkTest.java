74bd4d5d27d8c8295b73bf3ad9490a6898608851
Tobias Gauss <tobias.gauss@bosch.io>
2020-09-29 09:29:28 +0200

    Feedback incorporated (Result collector added, Test now fails if one of the JMeter samples fails). Fixes #2604
    Signed-off-by: Tobias Gauss <tobias.gauss@bosch.io>

--- repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/benchmark/ApiBenchmarkTest.java
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/benchmark/ApiBenchmarkTest.java
@@ -15,6 +15,8 @@ package org.eclipse.vorto.repository.server.benchmark;
 import com.google.common.collect.Sets;
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.gui.action.HtmlReportGenerator;
+import org.apache.jmeter.reporters.ResultCollector;
+import org.apache.jmeter.samplers.SampleEvent;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
@@ -41,7 +43,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -54,17 +55,29 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.utility.MountableFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import static org.eclipse.vorto.repository.domain.NamespaceRole.DEFAULT_NAMESPACE_ROLES;
 import static org.eclipse.vorto.repository.domain.RepositoryRole.DEFAULT_REPOSITORY_ROLES;
 
+/**
+ * Spins up a repository and corresponding testcontainers (mysql and elasticsearch) and runs a jmeter test plan
+ * (src/test/resources/benchmarkTests/apiCallGetModel.jmx). The results are stored and an HTML report is generated in:
+ * target/test-classes/benchmarkTests/report.
+ *
+ * If you want to execute the test directly in your IDE you need to add {@code clean resources:testResources jmeter:configure -Pbenchmark-tests}
+ * to your run configuration or make sure that you download JMeter manually and adapt the JMeter home path.
+ *
+ * Since the test data is compressed the testcontainer needs internet connectivity to install decompress-uitls via apt.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles(profiles={"local-benchmark-test"})
 @SpringBootTest(classes = VortoRepository.class,
@@ -98,6 +111,8 @@ public class ApiBenchmarkTest {
     @ClassRule
     public static JdbcDatabaseContainer mySQLContainer = (JdbcDatabaseContainer) new MySQLContainer("mysql:5.6.44").withDatabaseName("vorto_schema").withPassword("vorto").withUsername("vorto").withCommand("--max-allowed-packet=67108864");
 
+    private JUnitTestResultCollector jUnitTestResultCollector =  new JUnitTestResultCollector();
+
     // JMeter home dir
     public static final String JMETER_HOME_DIR = "benchmarkTests";
     // JMeter bin dir
@@ -125,9 +140,7 @@ public class ApiBenchmarkTest {
     }
 
     private static void globalJMeterConfiguration() throws URISyntaxException, IOException {
-
         BENCHMARK_PATH_STRING = Paths.get(ApiBenchmarkTest.class.getClassLoader().getResource(JMETER_HOME_DIR).toURI()).toString();
-
 
         // Initialize Properties
         JMeterUtils.loadJMeterProperties(BENCHMARK_PATH_STRING + "/user.properties");
@@ -145,15 +158,6 @@ public class ApiBenchmarkTest {
         return "opt/JMeter";
     }
 
-    @Before
-    public void setUpTest() throws IOException, URISyntaxException {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-        mock.setAuthorityListForUser(SpringUserUtils.toAuthorityList(
-                Sets.newHashSet(DEFAULT_NAMESPACE_ROLES[0], DEFAULT_NAMESPACE_ROLES[5], DEFAULT_NAMESPACE_ROLES[1], DEFAULT_NAMESPACE_ROLES[2], DEFAULT_NAMESPACE_ROLES[3], DEFAULT_NAMESPACE_ROLES[4], DEFAULT_REPOSITORY_ROLES[0])), "user1");
-        configureJMeter();
-    }
-
     /**
      * Configure JMeter execution. Set vorto repository port and load the testplan.
      *
@@ -169,6 +173,8 @@ public class ApiBenchmarkTest {
         JMeterUtils.setProperty(VORTO_RESULT_FILE_SUMMARY_PROPERTY, BENCHMARK_PATH_STRING  + "/resultsummary.csv");
         // load the test plan
         testPlanTree = SaveService.loadTree(Paths.get(ApiBenchmarkTest.class.getClassLoader().getResource(JMETER_HOME_DIR+"/apiCallGetModel.jmx").toURI()).toFile());
+        // add custom test result collector which fails the unit test if at least one jmeter sample fails
+        testPlanTree.add(testPlanTree.getArray()[0], jUnitTestResultCollector);
     }
 
     private static void setSystemProperties() {
@@ -215,11 +221,35 @@ public class ApiBenchmarkTest {
         }
     }
 
+    @Before
+    public void setUpTest() throws IOException, URISyntaxException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        mock.setAuthorityListForUser(SpringUserUtils.toAuthorityList(
+                Sets.newHashSet(DEFAULT_NAMESPACE_ROLES[0], DEFAULT_NAMESPACE_ROLES[5], DEFAULT_NAMESPACE_ROLES[1], DEFAULT_NAMESPACE_ROLES[2], DEFAULT_NAMESPACE_ROLES[3], DEFAULT_NAMESPACE_ROLES[4], DEFAULT_REPOSITORY_ROLES[0])), "user1");
+        configureJMeter();
+    }
+
     @Test
-    @WithMockUser(username = "user1", authorities = {"sysadmin","model_viewer","model_creator","namespace_admin"})
     public void testGetModelApi() {
         myJMeterEngine.configure(testPlanTree);
         myJMeterEngine.run();
+        if(!jUnitTestResultCollector.getFailedSamplesSet().isEmpty()) {
+            Assert.fail("Test failed because at least one instance of the following JMeter samples failed: " + jUnitTestResultCollector.getFailedSamplesSet().toString());
+        }
+    }
+
+    @After
+    public void createHtmlReport() throws IOException {
+        renameJar();
+        HtmlReportGenerator generator = new HtmlReportGenerator(JMeterUtils.getProperty(VORTO_RESULT_FILE_SUMMARY_PROPERTY),BENCHMARK_PATH_STRING + "/user.properties",BENCHMARK_PATH_STRING + "/report");
+        List<String> errorList = generator.run();
+        if(!errorList.isEmpty()) {
+            System.err.println("unable to create report due to errors: ");
+            for (String error: errorList) {
+                System.err.println(error);
+            }
+        }
     }
 
     /**
@@ -235,7 +265,6 @@ public class ApiBenchmarkTest {
             TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
                     applicationContext, "spring.datasource.password=" + mySQLContainer.getUsername());
             loadTestData();
-
         }
 
         /**
@@ -254,16 +283,22 @@ public class ApiBenchmarkTest {
         }
     }
 
-    @After
-    public void createHtmlReport() throws IOException {
-        renameJar();
-        HtmlReportGenerator generator = new HtmlReportGenerator(JMeterUtils.getProperty(VORTO_RESULT_FILE_SUMMARY_PROPERTY),BENCHMARK_PATH_STRING + "/user.properties",BENCHMARK_PATH_STRING + "/report");
-        List<String> errorList = generator.run();
-        if(!errorList.isEmpty()) {
-            System.err.println("unable to create report due to errors: ");
-            for (String error: errorList) {
-                System.err.println(error);
-            }
+    /**
+     * Special result collector which only collects the names of failed samples in JMeter.
+     */
+    private class JUnitTestResultCollector extends ResultCollector {
+
+        private HashSet<String> failedSamplesSet = new HashSet<>();
+
+        @Override
+        public void sampleOccurred(SampleEvent e) {
+            if(e.getResult() != null)
+                if(!e.getResult().isSuccessful())
+                    failedSamplesSet.add(e.getResult().getSampleLabel());
+        }
+
+        Set<String> getFailedSamplesSet() {
+            return failedSamplesSet;
         }
     }
 
