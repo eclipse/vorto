dee81d1c65e60792973590f00f7fb0d1c172c5a5
Kevin Olotu <kevin.olotu@bosch.io>
2020-10-02 09:57:06 +0200

    reconfigured createdby procedure
    Signed-off-by: Kevin Olotu <kevin.olotu@bosch.io>

---
+++ repository/repository-server/src/main/resources/db/changelog/migration/createdby_procedure.sql
@@ -0,0 +1,19 @@
+create procedure create_and_populate_createdby()
+begin
+    # verifies column does not exist already - aborts if it does
+    declare createdby tinyint;
+    set createdby = (
+        select count(*) from information_schema.COLUMNS where TABLE_NAME='user' and COLUMN_NAME='created_by'
+    );
+    if (createdby = 0) then
+        # adds the column
+        alter table user add column created_by bigint(20);
+        # no FK as deleting a user should succeed if they have created technical users, and not
+        # automatically cascade to them either
+        # populates with default data: every user is "created by themselves" by default, including
+        # technical users, since we cannot reliably verify that for already created users
+        update user set user.created_by = user.id;
+    end if;
+
+
+end;
