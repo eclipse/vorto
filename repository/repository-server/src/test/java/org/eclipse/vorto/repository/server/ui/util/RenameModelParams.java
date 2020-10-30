0a0e432f1f247c5c9828558708e0d01970d6414f
Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>
2020-10-30 15:18:29 +0100

    ui_test_enrichment
    - Latest formatting changes messed with the license header format
    
    Signed-off-by: Menahem Julien Raccah Lisei <menahemjulien.raccahlisei@bosch-si.com>

--- repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/ui/util/RenameModelParams.java
+++ repository/repository-server/src/test/java/org/eclipse/vorto/repository/server/ui/util/RenameModelParams.java
@@ -1,12 +1,13 @@
 /**
  * Copyright (c) 2020 Contributors to the Eclipse Foundation
- * <p>
- * See the NOTICE file(s) distributed with this work for additional information regarding copyright
- * ownership.
- * <p>
- * This program and the accompanying materials are made available under the terms of the Eclipse
- * Public License 2.0 which is available at https://www.eclipse.org/legal/epl-2.0
- * <p>
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0
+ *
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.vorto.repository.server.ui.util;
