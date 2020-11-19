ef81a221290c66f3ab422f5f07c73132cf676e97
Kevin Olotu <kevin.olotu@bosch.io>
2020-11-19 18:32:48 +0100

    added config for java11 deployment
    Signed-off-by: Kevin Olotu <kevin.olotu@bosch.io>

--- build-scripts/repackage-deploy-repo.sh
+++ build-scripts/repackage-deploy-repo.sh
@@ -42,6 +42,10 @@ elif [[ "$GIT_BRANCH" == "development" ]]
 then
   aws s3 cp s3://$VORTO_S3_BUCKET/configuration_files/dev ./BOOT-INF/classes --recursive
   aws s3 cp s3://$VORTO_S3_BUCKET/ebextensions/dev ./ --recursive
+elif [[ "$GIT_BRANCH" == "java11" ]]
+then
+  aws s3 cp s3://$VORTO_S3_BUCKET/configuration_files/dev ./BOOT-INF/classes --recursive
+  aws s3 cp s3://$VORTO_S3_BUCKET/ebextensions/dev ./ --recursive
 else
   echo "no extra files are include from S3Bucket"
 fi
